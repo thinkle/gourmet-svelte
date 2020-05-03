d718ede3686abf6dd2e2de341b04f21eb936c56b
Tom Hinkle <tmhinkle@gmail.com>
2020-05-02 22:41:30 -0400

    Widgets for editing

---
+++ src/webapp/recDisplay/RecBlock.svelte
@@ -0,0 +1,99 @@
+<script>
+ export let value
+ export let prop
+ export let editable=true;
+ export let forceEdit=false;
+ export let showLabel=true;
+ import RecDef from '../../common/RecDef.js';
+ import RichText from '../../widgets/RichText.svelte';
+
+ var displayValue;
+ $: displayValue = prop.toHtml && prop.toHtml(value) || value
+ let initialValue;
+
+ $: {
+     if (!editedThisTime) {
+         console.log('Resetting initialValue to ',displayValue);
+         initialValue = displayValue;
+     }
+ }
+ 
+ let editedThisTime = false;
+ 
+ var edit;
+ var editOn = false;
+ $: edit = editOn || forceEdit;
+
+ $: {
+     console.log('Edit',edit,'editOn',editOn,'force',forceEdit)
+ }
+
+ $: {
+     if (!edit) {
+         console.log('Not yet edited - setting');
+         editedThisTime = false
+     }
+ }
+ 
+ function turnEditOn () {
+     initialValue = displayValue 
+     console.log('Edit on - was ',editOn)
+     editOn = true
+ }
+ function turnEditOff () {
+     console.log('Edit OFf - was ',editOn)
+     editOn = false;
+     editedThisTime = false;
+ }
+
+ function handleChange (event) {
+     if (!editedThisTime) {console.log('Setting editedThisTime to true!')}
+     editedThisTime = true;
+     value = event.detail || event.target.value;
+ }
+ 
+</script>
+<div>
+    <div class="top">
+        <h3>
+            {#if showLabel}
+            {prop.label}
+            {/if}
+        </h3>
+        {#if editable && !edit}
+        <button class="icon" on:click={turnEditOn}>
+            <i class="material-icons">edit</i>
+        </button>
+        {/if}
+        {#if edit && !forceEdit}
+        <button class="icon" on:click={turnEditOff}>
+            <i class="material-icons">close</i>
+        </button>
+        {/if}
+    </div>
+    {#if edit}
+    <RichText initialValue={initialValue}
+              on:change="{handleChange}"
+    />
+    {:else}
+    <div>{@html displayValue}</div>
+    {/if}
+
+</div>
+<style>
+ div {
+     display: block;
+     max-width: 100%;
+ }
+ .top {
+     display: flex;
+     margin-bottom: 0.5em;
+ }
+ .top button {
+     align-self: flex-start;
+     margin-left: auto;
+     margin-bottom: 0;
+     margin-right: 5px;
+ }
+ 
+</style>
---
+++ src/webapp/recDisplay/RecProp.svelte
@@ -0,0 +1,119 @@
+<script>
+ export let value
+ export let prop
+ export let editable
+ export let forceEdit
+ export let showLabel=true;
+ import RecDef from '../../common/RecDef.js';
+ import PlainInput from '../../widgets/PlainInput.svelte';
+ import DurationInput from '../../widgets/DurationInput.svelte';
+ import UrlInput from '../../widgets/UrlInput.svelte';
+ import NumberUnitInput from '../../widgets/NumberUnitInput.svelte';
+ let modes = RecDef.EditModes
+ let ref;
+ const propInput = {
+     [modes.TXT] : PlainInput,
+     [modes.RCH] : PlainInput,
+     [modes.CMB] : PlainInput,
+     [modes.DUR] : DurationInput,
+     [modes.CMB] : PlainInput,
+     [modes.LNK] : UrlInput,
+     [modes.NUMUNIT] : NumberUnitInput,
+ }
+ console.log('propInput:',propInput);
+ var displayValue;
+ $: displayValue = prop.toHtml && prop.toHtml(value) || value
+
+ var edit;
+ var editOn = false;
+ $: edit = editOn || forceEdit;
+
+ $: {
+     console.log('Edit',edit,'editOn',editOn,'force',forceEdit)
+ }
+ 
+ function turnEditOn () {
+     console.log('Edit on - was ',editOn)
+     editOn = true
+ }
+ function turnEditOff () {
+     console.log('Edit OFf - was ',editOn)
+     editOn = false;
+ }
+
+ function handleChange (event) {
+     value = event.detail || event.target.value;
+ } 
+ 
+</script>
+<div>
+    <div>
+        {#if showLabel}
+        <div class="top" >
+            <label on:click={()=>ref.focus()}>{prop.label}</label>
+            {#if editable && !edit}
+            <button class="icon" on:click={turnEditOn}>
+                <i class="material-icons">edit</i>
+            </button>
+            {/if}
+            {#if edit && !forceEdit}
+            <button class="icon" on:click={turnEditOff}>
+                <i class="material-icons">done</i>
+            </button>
+            {/if}
+        </div>
+        {/if}
+        <div>
+            {#if edit}
+            <svelte:component this={propInput[prop.edit]}
+                              on:change={handleChange}
+                              displayValue={displayValue}
+                              value={value}
+                              bind:this={ref}
+            />
+            {:else}
+            <b>{@html displayValue}</b>
+            {/if}
+            {#if !showLabel}
+              {#if editable && !edit}
+              <button class="icon" on:click={turnEditOn}>
+                  <i class="material-icons">edit</i>
+              </button>
+              {/if}
+              {#if edit && !forceEdit}
+              <button class="icon" on:click={turnEditOff}>
+                  <i class="material-icons">done</i>
+              </button>
+              {/if}
+           {/if}
+        </div>
+    </div>
+
+</div>
+<style>
+ div {
+     display: block;
+ }
+ .top {
+     display: flex;
+     width: 100%;
+     align-items: center;
+ }
+ .top button {
+     margin-left: 1em;
+ }
+
+ b {
+     margin-right: 0.5em;
+ }
+ input {
+     margin-bottom: 0;
+ }
+ button {
+     align-self: flex-end;
+     margin-bottom: 0}
+ input {
+     color: var(--black);
+     background-color: var(--white);}
+
+</style>
