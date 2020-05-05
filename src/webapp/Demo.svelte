314ab0622c5388cba6cccaa1fcdc08415062e5e4
Tom Hinkle <tmhinkle@gmail.com>
2020-05-05 09:25:53 -0400

    Support default demo routes

--- src/webapp/Demo.svelte
+++ src/webapp/Demo.svelte
@@ -5,15 +5,33 @@
  import RecDisplayTests from './recDisplay/DemoRecDisplay.svelte';
  import WidgetTests from '../widgets/WidgetTests.svelte';
  import LocalDataTester from './LocalDataTester.svelte';
+ import RecipeDataTester from '../stores/RecipeDataTester.svelte';
+ export let demo
+ 
+ let demos = {
+     rec : RecDisplayTests,
+     localData : LocalDataTester,
+     widgets : WidgetTests,
+     tester : Tester,
+     rd : RecipeDataTester,
+ }
+
+ $: {
+     console.log('Demo is',demo);
+ }
 </script>
 <div>
-    <Tester name="Widgest" >
+    {#if demo}
+    <svelte:component this="{demos[demo]}" initialShow={true} />
+    {:else}
+    <Tester name="Widgets" >
         <WidgetTests/>
     </Tester>
     <Tester name="Recipe Display" >
         <RecDisplayTests/>
     </Tester>
     <LocalDataTester/>
+    {/if}
 </div>
 <style>
 </style>
