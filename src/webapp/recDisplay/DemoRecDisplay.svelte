283f75d298e0b833e87fb1b70297c983b54699f7
Tom Hinkle <tmhinkle@gmail.com>
2020-05-05 09:26:04 -0400

    support initialShow param

--- src/webapp/recDisplay/DemoRecDisplay.svelte
+++ src/webapp/recDisplay/DemoRecDisplay.svelte
@@ -2,12 +2,14 @@
  import Tester from '../../widgets/Tester.svelte';
  import {testRecs} from '../../common/mocks/recipes.js'
  import Recipe from './Recipe.svelte';
+ export let initialShow
 </script>
 
-<div>
+<div> 
     <Tester name="Test Recipe"
             options={Object.keys(testRecs)}
             let:option={recname}
+            initialShow={initialShow}
     >
         <Recipe rec={testRecs[recname]}>
         </Recipe>
