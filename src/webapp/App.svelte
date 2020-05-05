36b664e48d173ee45114009f5f9392f61c7168cd
Tom Hinkle <tmhinkle@gmail.com>
2020-05-05 09:25:23 -0400

    ++RecipeList is the new landing page

--- src/webapp/App.svelte
+++ src/webapp/App.svelte
@@ -1,24 +1,31 @@
 <script>
  import router from 'page';
  import Recipe from './recDisplay/Recipe.svelte';
+ import RecipeList from './recDisplay/RecipeList.svelte';
  import Demo from './Demo.svelte';
  import {testRecs} from '../common/mocks/recipes.js'
 
 
  let name = 'Gourmet';
  let page;
- let params;
- router('/',()=>{page = Recipe; params={rec:testRecs.standard}});
- router('/demo',()=>page = Demo);
+ let params = {};
+ router('/',()=>{page = RecipeList; params={}});
+ router('/oneRec',()=>{page = Recipe; params={rec:testRecs.standard}});
+ router('/demo/:demo',(ctx)=>{
+     console.log('Demo...',ctx);
+     page = Demo
+     params = ctx.params
+ });
+
  router.start();
  $: {
      console.log('params:',params);
+     console.log('page:',page);
  }
 </script>
 
 <div>
-
-<svelte:component this={page} {...params}/>
+    <svelte:component this={page} {...params}/>
 </div>
 <style>
  .app {
