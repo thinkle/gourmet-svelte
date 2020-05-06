dc8e07a81e5bd79f1de990d1e485001a78ac557c
Tom Hinkle <tmhinkle@gmail.com>
2020-05-05 22:09:07 -0400

    test api user credential magic

--- src/webapp/Landing.svelte
+++ src/webapp/Landing.svelte
@@ -2,11 +2,24 @@
  import RecipeList from './recDisplay/RecipeList.svelte';
  import netlifyIdentity from 'netlify-identity-widget'
  import { user, redirectURL } from '../stores/user.js'
- 
+ let apiTest
  netlifyIdentity.init();
 
  $: isLoggedIn = !!$user
  $: username = $user !== null ? $user.username : ' there!'
+ let url = "/.netlify/functions/api?message=howdy"
+
+ function testApi () {
+     console.log('test the api');
+     apiTest = fetch(url, {
+         headers: {
+             Accept: "application/json",
+             "Content-Type": "application/json",
+             Authorization: "Bearer " + ($user && $user.token && $user.token.access_token), // like this
+         },
+     })
+     console.log('apiTest is now a promise?',apiTest);
+ }
 
  function doLogout () {
      user.logout()
@@ -48,5 +61,25 @@
             <button on:click={() => doSignup() }>Sign Up</button>
         </div>
     {/if}
+    <button on:click={testApi}>Test API</button>
+    <div>
+        {#await apiTest}
+            Fetching data from {url} with access token {user && user.token && user.token.access_token}
+        {:then response}
+            {#if response}
+                {#await response.text()}
+                    Fetching text...
+                {:then text}
+                    Yippee got a response:
+                    {JSON.stringify(text)}
+                {:catch error}
+                    Bummer... error fetching text {JSON.stringify(error)}
+                {/await}
+            {/if}
+        {:catch error}
+            Bummer got an error
+            {JSON.stringify(error)}
+        {/await}
+    </div>
     <RecipeList/>
 </div>
