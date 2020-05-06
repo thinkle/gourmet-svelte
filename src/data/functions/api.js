2bec683a33d01cf255dae74f5d308e04d97c422f
Tom Hinkle <tmhinkle@gmail.com>
2020-05-05 22:08:35 -0400

    Get user info

--- src/data/functions/api.js
+++ src/data/functions/api.js
@@ -3,9 +3,14 @@
 const handler = async (event, context) => {
     let params = event.queryStringParameters;
     console.log('hello world',params)
+    const {
+        // this magic documented here:
+        // https://www.gatsbyjs.org/blog/2018-12-17-turning-the-static-dynamic/#bonus-points-authenticated-lambda-functions-for-your-gatsby-app
+        user, // actual user info you can use for your serverless functions
+    } = context.clientContext
     return {
         statusCode:200,
-        body:`Got params: ${JSON.stringify(params)}`
+        body:`Params: ${JSON.stringify(params)} and user ${user}`
     }
 }
 
