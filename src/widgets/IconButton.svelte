e1b276d0e8b923b6618676f2134d27a21c9c1450
Tom Hinkle <tmhinkle@gmail.com>
2020-05-13 13:54:16 -0400

    ++Icon styling
    This had all been in global, but global doesn't get wrapped up with our chrome extension

--- src/widgets/IconButton.svelte
+++ src/widgets/IconButton.svelte
@@ -16,5 +16,20 @@
  .inverse {
      background-color: #232323;
      color: white;
-}
+ }
+ button.icon {
+     border: none;
+     background-color: transparent;
+     border-radius: 10px;
+     transition: all 0.8s;
+ }
+ button.icon:hover {
+     background-color: #7474f4;
+     color: white;
+ }
+ button.icon:focus {
+     border: 1px solid #7474f4;
+ }
+
+
 </style>
