c0fe569dd10f683ff374042d6ab6df3af0195a3d
Tom Hinkle <tmhinkle@gmail.com>
2020-05-13 09:30:35 -0400

    ++Convenience class for icon buttons

---
+++ src/widgets/IconButton.svelte
@@ -0,0 +1,20 @@
+<script>
+ let b;
+ export let inverse=false;
+ export function focus () {
+     b.focus()
+ }
+</script>
+
+<button class:inverse class="icon" on:click bind:this={b}>
+    <i  class="material-icons">
+        <slot>
+        </slot>
+    </i>
+</button>
+<style>
+ .inverse {
+     background-color: #232323;
+     color: white;
+}
+</style>
