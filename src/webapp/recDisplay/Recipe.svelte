83cb2f29fbc57337d3481edb4f183d6ce26df42c
Tom Hinkle <tmhinkle@gmail.com>
2020-05-13 09:32:28 -0400

    Multipliable + use context API to decide when to check for updates to our object.

--- src/webapp/recDisplay/Recipe.svelte
+++ src/webapp/recDisplay/Recipe.svelte
@@ -4,25 +4,36 @@
  import RecProp from './RecProp.svelte';
  import SideBySide from '../../widgets/SideBySide.svelte';
  import RecBlock from './RecBlock.svelte';
+ import AmountInput from '../../widgets/AmountInput.svelte';
  import {recipeData,recipeActions,connected,updateCount} from '../../stores/recipeData.js';
- import {onMount} from 'svelte'
+ import {onMount,setContext} from 'svelte'
+ import {writable} from 'svelte/store'
  let changed;
 
  export let rec={ingredients : [],
                 text : [],
                 images : [],
                 };
+
  export var editMode = false;
  export let editable = true;
  var ingeditmode = false;
  
- $: if (rec.id) {
+ 
+ // Store business...
+ let multiplier = writable(1);
+ setContext('multiplier',multiplier);
+ let recipeChanges = writable(0);
+ setContext('recipeChanges',recipeChanges);
+ let lastUpdateCheck = 0;
+ $: if ($recipeChanges > lastUpdateCheck) {
      console.log('Recipe calls updateCurrent');
      recipeActions.updateCurrent(rec.id,rec)
+     lastUpdateCheck = $recipeChanges;
  }
  
-     
-     
+
+
 </script>
 
 <div>
@@ -34,7 +45,13 @@
                 </span>
             {/each}        
         </h2>
-        <span>Update: {$updateCount[rec.id]}</span>
+        <div style="width:6em">
+            &times;
+            <AmountInput value={$multiplier} on:change={(e)=>$multiplier=e.detail}
+                         showPlusMinusButtons={true}
+            />
+        </div>
+        <span>Change Events: {$recipeChanges}. Update: {$updateCount[rec.id]}</span>
         <button class="toggle" class:active-toggle={editMode} on:click={()=>editMode=!editMode}>
             Edit{#if editMode}ing{/if}
             Recipe <i class="material-icons">edit</i></button>
@@ -46,7 +63,7 @@
             </button>
         {/if}
     </div>
-    <SideBySide height="60vh">
+    <SideBySide height="80vh" growRight="true" leftBasis="300px" rightBasis="600px">
 	<h3 slot="leftHead">
 	    Ingredients
             {#if !editMode}
@@ -63,20 +80,26 @@
 	</div>		
 	
 	<div slot="right">
-            {#each rec.images as image}
-                <!-- Small: <img alt={image.alt||rec.title} src={image.thumbnailUrl}/> -->
-                Big: <img alt={image.alt||rec.title} src={image.url}/>
-            {/each}
-            {#each RecDef.recProps as prop}
-                {#if rec.hasOwnProperty(prop.name)}
-                    <RecProp editable={editable} forceEdit={editMode} prop={prop} bind:value={rec[prop.name]}/>
-                {/if}
-                {#if editMode}
-                    {#if !rec.hasOwnProperty(prop.name)}
-                        <div class="small"><button on:click={()=>rec[prop.name]=prop.empty}>Add {prop.label}?</button></div>
-                    {/if}
-                {/if}
-            {/each}
+            <div class="topblock" >
+                <div class="images" >
+                    {#each rec.images as image}
+                        <!-- Small: <img alt={image.alt||rec.title} src={image.thumbnailUrl}/> -->
+                        <img alt={image.alt||rec.title} src={image.url}/>
+                    {/each}
+                </div>
+                <div class="props" >
+                    {#each RecDef.recProps as prop}
+                        {#if rec.hasOwnProperty(prop.name)}
+                            <RecProp editable={editable} forceEdit={editMode} prop={prop} bind:value={rec[prop.name]}/>
+                        {/if}
+                        {#if editMode}
+                            {#if !rec.hasOwnProperty(prop.name)}
+                                <div class="small"><button on:click={()=>rec[prop.name]=prop.empty}>Add {prop.label}?</button></div>
+                            {/if}
+                        {/if}
+                    {/each}
+                </div>
+            </div>
             {#each rec.text as textBlock}
                 <RecBlock bind:value="{textBlock}" forceEdit="{editMode}"/>
             {/each}
@@ -99,4 +122,11 @@
  .top button {
      margin-right: 1em;
  }
+ .images {
+     float : right;
+ }
+ .props {
+     display: inline-block;
+ }
+ 
 </style>
