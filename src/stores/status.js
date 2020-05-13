09d49b944437058a4a1e046b6fc8106ebadac2f9
Tom Hinkle <tmhinkle@gmail.com>
2020-05-13 09:42:42 -0400

    ++Store for updating status info

---
+++ src/stores/status.js
@@ -0,0 +1,74 @@
+import { writable, get } from 'svelte/store';
+
+const { subscribe, update } = writable({});
+
+let count = 0;
+
+export let states = {
+    INITIAL : 'Ready to start',
+    STARTED : 'Started',
+    PROGRESS : 'In progress',
+    ERROR : 'Error',
+    COMPLETE : 'Complete'
+}
+
+export default {
+    subscribe,
+
+    createStatus (name,metadata={}) {
+        count += 1;
+        let id = count;
+        update(
+            (data)=>{
+                data[id] = {
+                    id,
+                    name,
+                    ...metadata,
+                    status: states.INITIAL
+                }
+                return data;
+            }
+        );
+        return id;
+    },
+
+    removeStatus (id) {
+        update(
+            (data)=>{
+                delete data[id];
+                return data
+            });
+        return id;
+    },
+
+
+    start (id,props={}) {
+        return this.updateStatus(id,states.STARTED,props)
+    },
+    progress (id,props={}) {
+        return this.updateStatus(id,states.PROGRESS,props)
+    },
+    error (id,error) {
+        return this.updateStatus(id,states.PROGRESS,{error})
+    },
+    complete (id,complete,props={}) {
+        return this.updateStatus(id,states.COMPLETE,props)
+    },
+
+    updateStatus (id, statusCode, extraData) {
+        update(
+            (data)=>{
+                data[id].status = statusCode
+                data[id] = {
+                    ...data[id],
+                    ...extraData
+                }
+                return data;
+            }
+        );
+    }
+    
+    
+}
+    
+
