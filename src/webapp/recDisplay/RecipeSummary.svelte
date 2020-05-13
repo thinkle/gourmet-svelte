6791113b6006eae5c78845d29f0108b195211922
Tom Hinkle <tmhinkle@gmail.com>
2020-05-13 09:32:58 -0400

    Little view for listing recipes

---
+++ src/webapp/recDisplay/RecipeSummary.svelte
@@ -0,0 +1,32 @@
+<script>
+ import RecDef from '../../common/RecDef.js';
+ import RecProp from './RecProp.svelte';
+ export let recipe
+ export let onClick
+ let thumb;
+ $: thumb = recipe.images && recipe.images.length > 0 && recipe.images[0];
+ function wowAClick (event) {
+     console.log('A click!');
+     if (onClick) {
+         console.log('Calling callback...');
+         onClick(event);
+     }
+ }
+</script>
+<tr class='summary' on:click={wowAClick}>
+    <td>
+        {#if thumb}
+            <img src={thumb.thumbnailUrl||thumb.url} alt={thumb.alt||recipe.title}/>
+        {/if}
+    </td>
+    <td>
+        <h3>{recipe.title}</h3>
+    </td>
+    {#each RecDef.recProps.filter((p)=>p.summaryView) as prop}
+        <td>
+            <RecProp showLabel={false} editable={false} prop={prop} value={recipe[prop.name]}></RecProp>
+        </td>
+    {/each}        
+</tr>
+<style>
+</style>
