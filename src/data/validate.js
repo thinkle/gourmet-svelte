6c924743395156dcec89443d86d8824a99786cc5
Tom Hinkle <tmhinkle@gmail.com>
2020-05-13 08:53:29 -0400

    work to prepare local recipe for indexing by indexeddb

--- src/data/validate.js
+++ src/data/validate.js
@@ -1,5 +1,6 @@
 // Validate and prepare our recipe for insertion...
 // This may do DB-specific things to the recipe to make life easier...
+import stopword from 'stopword';
 
 function validateRec (rec) {
     rec.flatIngredients = []
@@ -10,16 +11,16 @@ function validateRec (rec) {
 function getFullText (rec) {
     let texts = [];
     texts.push(rec.title||'')
-    rec.text.forEach(
+    rec.text && rec.text.forEach(
         (text)=>texts.push(removeMarkup(text.body))
     );
     rec.flatIngredients.forEach(
         (ing)=>texts.push(ing.text||'')
     );
-    rec.sources.forEach(
+    rec.sources && rec.sources.forEach(
         (source)=>texts.push(source.name||'')
     );
-    rec.categories.forEach(
+    rec.categories && rec.categories.forEach(
         (cat)=>texts.push(cat.name)
     );
     return texts.join(' ');
@@ -71,7 +72,37 @@ export function prepRecs (recs,user) {
         );
     }
 }
+let commonWords = ['the','and','but','for']
 
+export function prepRecLocal (rec) {
+    validateRec(rec); // step 1...
+    rec.words = [];
+    rec.fullText.split(/\W+/).forEach(
+        (w)=>{
+            w = w.toLowerCase();
+            if (!commonWords.indexOf(w)>-1 &&
+                rec.words.indexOf(w)==-1) {
+                rec.words.push(w)
+            }
+        }
+    );
+    delete rec.fullText; // we don't need to keep this.
+    rec.words = stopword.removeStopwords(rec.words);
+    rec.ings = []
+    rec.flatIngredients.forEach(
+        (i)=>{
+            if (i.text) {rec.ings.push(i.text.toLowerCase());}
+            if (i.ingkey) {
+                rec.ings.push(i.ingkey.toLowerCase());
+            }
+            if (i.text.split().length > 1) {
+                rec.ings = [...rec.ings,...i.text.split()];
+            }
+        });
+    rec.ings = stopword.removeStopwords(rec.words);
+    delete rec.flatIngredients // not keeping this either
+    return rec;
+}
 
 export {validateRec}
 
