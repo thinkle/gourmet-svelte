d5f4cf4b10575770612769eab165852bc654a324
Tom Hinkle <tmhinkle@gmail.com>
2020-05-13 09:42:24 -0400

    ++Formatting clean-up

--- src/widgets/HtmlBlock.svelte
+++ src/widgets/HtmlBlock.svelte
@@ -7,13 +7,8 @@
  
  function crawlNode (node) {
      if (markupToComponentMap[node.tagName]) {
-	 components.push({node,
-											                                            type:markupToComponentMap[node.tagName]
+	 components.push({node,type:markupToComponentMap[node.tagName]
 			 })
-	 console.log('build',node.tagName);
-     }
-     else {
-	 console.log('ignore',node.tagName)
      }
      for (let child of node.children) {
 	 crawlNode(child);
@@ -21,34 +16,42 @@
  }
  
  $: {
-     console.log('Parsing and crawling')
      parsed =  new DOMParser().parseFromString(content, 'text/html');
      crawlNode(parsed.body);	
  }
  
- $: {if (root) {addToRoot()}}
+ $: {
+     if (root) {
+         addToRoot()
+     }
+ }
 	          
-                  function addToRoot () {
-		      console.log('Adding to root element')
-		      components.forEach(
-			  (c)=>c.component.addToDom(c.node)
-		      );
-		      root.appendChild(parsed.body)
-		  }
+ function addToRoot () {
+     components.forEach(
+	 (c)=>c.component.addToDom(c.node)
+     );
+     root.appendChild(parsed.body)
+ }
  
  function mapAttrs (attributes) {
-     const map = {}
-		                                                                                                                    for (let a of attributes) {
-			                                                                                                                map[a.name]=a.value
-		                                                                                                                    }
-		                                                                                                                    return map
-	         }
-	 
-	 let root;
+     const map = {
+     };
+     for (let a of attributes) {
+	 map[a.name]=a.value
+     }
+     return map
+ }
+ 
+ let root;
 </script>
 <div bind:this={root}>	
 </div>
-
+<!-- Components mounted below will be removed from their position and added to the DOM
+     by JavaScript. They must each support an addToDom method which takes a DOM parent
+     and adds them to it.
+-->
 {#each components as component,i}
-    <svelte:component this={components[i].type} bind:this={components[i].component} {...mapAttrs(components[i].node.attributes)} >{@html components[i].node.innerHTML}</svelte:component>
+    <svelte:component this={components[i].type} bind:this={components[i].component} {...mapAttrs(components[i].node.attributes)} >
+        {@html components[i].node.innerHTML}
+    </svelte:component>
 {/each}
