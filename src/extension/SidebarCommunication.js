50e8b8671b143161d944ed9be96fcccc3e130c4b
Tom Hinkle <tmhinkle@gmail.com>
2020-05-14 14:42:24 -0400

    Move code out to parser

--- src/extension/SidebarCommunication.js
+++ src/extension/SidebarCommunication.js
@@ -1,5 +1,4 @@
-var parsed = {
-}
+import {parsed} from './parser/backgroundParser.js';
 
 export function launchTab (tab) {
     console.log('browser action triggered: launch tab');
@@ -18,36 +17,3 @@ export function launchTab (tab) {
         });
 }
 
-export function handleContentRequest (request, sender, sendResponse) {
-    console.log('Got content request');
-    if (request.action=='removeTag') {
-        console.log('Deleting...');
-        delete parsed[sender.tab.id][request.id]
-        console.log(parsed);
-    }
-    if (request.action=='addTag') {
-        console.log('Adding tag (request from content)');
-        console.log('Request: %s',JSON.stringify(request));
-        if (!parsed[sender.tab.id]) {
-            parsed[sender.tab.id] = {}
-        }
-        parsed[sender.tab.id][request.message.id]=request.message;
-    }
-}
-
-export function handleSidebarRequest (request, sender, sendResponse) {
-    console.log('Sidebar message incoming');
-        console.log('Got message from sidebar: %s',
-                    JSON.stringify({
-                        request:request,
-                        sender:sender,
-                        sendResponse,sendResponse}));
-        console.log('Message from tab: %s',sender.tab.url);
-        sendResponse(
-            {
-                url : sender.tab.url,
-                tabid : request.tabId || sender.tab.id, // we accept either...
-                parsed : parsed[request.tabId||sender.tab.id],
-            }
-        );
-}
