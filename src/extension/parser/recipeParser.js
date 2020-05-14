7f753f163a25c57de43e6f9d8c670d1c5e17bc57
Tom Hinkle <tmhinkle@gmail.com>
2020-05-14 14:43:08 -0400

    ++ updates to nyt - add xpath

--- src/extension/parser/recipeParser.js
+++ src/extension/parser/recipeParser.js
@@ -1,86 +1,109 @@
-import $ from 'jquery';
-
 const defaultParsers = [
-    {el:'h1',tag:'title'},
-    {el:'.tag',tag:'category'},
-    {el:'.step',tag:'instructions'},
-    {el:'.ingredient',tag:'ingredient'},
-    {el:'.ing',tag:'ingredient'},
+    {selector:'h1',tag:'title'},
+    {selector:'.tag',tag:'category'},
+    {selector:'.step',tag:'text',detail:'Instructions'},
+    {selector:'.ingredient',tag:'ingredient'},
+    {selector:'.ing',tag:'ingredient'},
 ];
 
 const byDomain = {
     'cooking.nytimes.com':[
-        {el:'.recipe-title',tag:'title'},
-        {el:'.tag-block a',tag:'category'},
-        {el:'.byline',tag:'source'},
-        {el:'[itemProp="recipeYield"]',tag:'yield'},
-        {el:'.recipe-ingredients-wrap .part-name',tag:'inggroup'},
-        {el:'.quantity',tag:'amount'},
-        {el:'.ingredient-name',tag:'ingredient'},
-        {el:'[itemprop="recipeInstructions"]',tag:'instructions'},
-        {el:'.recipe-notes',tag:'footnote'},
-        {el:'.topnote',tag:'headnote'},
-        {el:'[itemprop="image"]',tag:'image'}, // fix me
+        {selector:'.recipe-title',tag:'title'},
+        {selector:'.tag-block a',tag:'category'},
+        {selector:'.byline',tag:'source'},
+        {selector:'[itemProp="recipeYield"]',tag:'yield'},
+        {selector:'.recipe-ingredients-wrap .part-name',tag:'inggroup'},
+        {selector:'.quantity',tag:'amount'},
+        {selector:'.ingredient-name',tag:'ingredient'},
+        {selector:'[itemprop="recipeInstructions"]',tag:'text',detail:'Instructions'},
+        {selector:'.recipe-notes',tag:'footnote'},
+        {selector:'.topnote',tag:'text'},
+        {selector:'.tag',tag:'category'},
+        {selector:'',tag:''},
+        {selector:'.recipe-yield-value',tag:'yield'},
+        {selector:'.recipe-steps',tag:'text'},
+        {selector:'[itemprop="image"]',tag:'image'}, // fix me
     ],
     'www.cooksillustrated.com':[
-        {el:'.detail__header--text',
+        {selector:'.detail__header--text',
          tag:'title'},
-        {el:'h2.document-header__title',
+        {selector:'h2.document-header__title',
          tag:'title'},
-        {el:'h2[name=title]',
+        {selector:'h2[name=title]',
          tag:'title'},
-        {el:'.ingredients',
+        {selector:'.ingredients',
          tag:'ingredients'},
-        {el:'.ingredients h5',
+        {selector:'.ingredients h5',
          tag:'inggroup'},
-        {el:'.recipe__ingredient--header',
+        {selector:'.recipe__ingredient--header',
          tag:'inggroup'},
-        {el:'.recipe__ingredient--quantity',
+        {selector:'.recipe__ingredient--quantity',
          tag:'amount'},
-        {el:'.recipe__ingredient--detail',
+        {selector:'.recipe__ingredient--detail',
          tag:'ingredient'},
-        {el:'div.long',
+        {selector:'div.long',
          tag:'footnote'},
-        {el:'section.asides',
-         tag:'modifications'},
-        {el:'section.recipe-instructions',
-         tag:'instructions'},
-        {el:'.recipe-instructions__yield',
+        {selector:'section.asides',
+         tag:'text',detail:'Notes'},
+        {selector:'section.recipe-instructions',
+         tag:'text',detail:'Instructions'},
+        {selector:'.recipe-instructions__yield',
          tag:'yield'},
-        {el:'.serves',
+        {selector:'.serves',
+         tag:'yield: Servings'},
+        {selector:'.recipe__yield',
          tag:'yield'},
-        {el:'.recipe__yield',
-         tag:'yield'},
-        {el:'#recipe_intro .detail__content',
-         tag:'headnote'},
-        {el:'#recipe_intro .detail__content',
-         tag:'headnote'},
-        {el:'.recipe__instructions--content',
-         tag:'instructions'},
-        {el:'.asides',
-         tag:'footnote'},
+        {selector:'#recipe_intro .detail__content',
+         tag:'text',detail:'Headnote'},
+        {selector:'#recipe_intro .detail__content',
+         tag:'text',detail:'Headnote'},
+        {selector:'.recipe__instructions--content',
+         tag:'text',detail:'Instructions'},
+        {selector:'.asides',
+         tag:'text',detail:'Footnote'},
+        /* 
+           <li class="recipe-yield-container">
+                <span class="recipe-yield-time-label recipe-yield">Yield</span>
+                <span class="recipe-yield-value">4 to 6 servings</span>
+              </li>
+
+        */
+        {xpath:'//span@recipe-yield-time-label[text()[contains(.,"Yield")]/following-sibling:span',
+         tag:'servings'},
+        /*<span class="recipe-yield-time-label recipe-time">Time</span>*/
+        {xpath:'//span@recipe-yield-time-label[text()[contains(.,"Time")]/following-sibling:span',
+         tag:'time'}
     ],
 }
 
 
 function Parser (addTag) {
-    console.log("CREATED PARSER WITH addTag=",addTag);
     var self = {
         
         auto_parse : function () {
             var domain = document.domain;
             var parsers = byDomain[domain]||defaultParsers;
             parsers.forEach((parser)=>{
-                self.maybe_add(parser.el,parser.tag)
+                self.maybe_add(parser)
             });
         },
         
-        maybe_add : function (selector, tag, ignoreSlug) {
-            console.log('maybe_add checking %s = %s',selector,tag)
-            $(selector).each(function (el) {
-                console.log('Add %s to %s',tag,this);
-                addTag(this,tag);
-            });
+        maybe_add : function ({selector, tag, ignoreSlug, xpath,detail}) {
+            if (xpath) {
+                let results = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null );
+                var result = results.iterateNext();
+                while (result) {
+                    console.log('Tag',result,tag,detail)
+                    addTag(result,tag,detail);
+                    result = results.iterateNext();
+                }
+            }
+            if (selector) {
+                document.querySelectorAll(selector).forEach(function (el) {
+                    console.log('Tag',el,tag,detail)
+                    addTag(el,tag,detail);
+                });
+            }
         }
     }
     return self;
