6e3b432c570e5be9ad07152b9a66c13969df1e09
Tom Hinkle <tmhinkle@gmail.com>
2020-05-14 14:42:31 -0400

    reorg

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,8 +1,12 @@
 import Metadata from '../common/RecDef.js';
-import {handleContentRequest, handleSidebarRequest, launchTab} from './SidebarCommunication.js';
+import {handleContentRequest,handleSidebarRequest} from './parser/backgroundParser.js';
+import {launchTab} from './SidebarCommunication.js';
+import ContextMenus from './ContextMenus.js';
 
 console.log('Background loaded...');
 chrome.runtime.onMessageExternal.addListener(handleSidebarRequest);
 chrome.runtime.onMessage.addListener(handleContentRequest);
 chrome.browserAction.onClicked.addListener(launchTab);
 console.log('We are listening...');
+ContextMenus.init();
+console.log('We made context menus...')
