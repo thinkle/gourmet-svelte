3cc2a3516948678f9a70e3966d301a8dbd18dcb8
Tom Hinkle <tmhinkle@gmail.com>
2020-05-14 14:39:11 -0400

    Render component in an iFrame sandbox

---
+++ src/extension/parser/ComponentSandbox.svelte
@@ -0,0 +1,58 @@
+<script>
+ let ifr
+ import {onMount,tick} from 'svelte';
+ let width
+ let height
+ let body
+ let componentContainer;
+ export let onResize
+ const wiggleRoom = 3; // a little extra space for size calculations...
+ 
+ onMount( ()=>{
+     ifr.onload = async function () {
+         console.log('Got doc?',ifr.contentDocument);
+         body = ifr.contentDocument
+                   .querySelector("div")
+         console.log('Got body!',body);
+         if (componentContainer && body) {
+             mountComponent();
+         }
+         width = body.clientWidth + wiggleRoom; // for good measure
+         height = body.clientHeight + wiggleRoom;
+         if (onResize) {onResize(width,height)}
+     }
+ });
+
+ function mountComponent () {
+     /* console.log('Mounting component, here we go!');
+      * console.log('Render component',component,'to',body,props); */
+     body.appendChild(componentContainer)
+     let ro = new ResizeObserver( entries => {
+         width = body.clientWidth + wiggleRoom
+         height = body.clientHeight + wiggleRoom
+         if (onResize) {
+             onResize(width,height)
+         }
+     });
+     ro.observe(body);
+ }
+
+</script>
+<iframe title="sandbox" srcdoc='<html><body style="margin:0;padding:0;"><div style="display:inline-block"></div></body></html>' bind:this={ifr} width={width} height="{height}"> 
+</iframe> 
+<div style="display:inline-block" bind:this={componentContainer}>
+    <slot/>
+</div>
+
+
+<style>
+    div {
+    position: absolute;
+    top: 10000px;
+    left: 10000px;
+    visibility: hidden;
+    }
+ iframe {
+     border: none;
+ }
+</style>
