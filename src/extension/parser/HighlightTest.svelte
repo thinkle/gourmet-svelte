ff6f04b44655c7bced7f1e55cddc12be8d4644aa
Tom Hinkle <tmhinkle@gmail.com>
2020-05-15 02:02:13 -0400

    ++hanyd tester library for highlighter

---
+++ src/extension/parser/HighlightTest.svelte
@@ -0,0 +1,36 @@
+<script>
+ import Tester from '../../widgets/Tester.svelte';
+ import tagger from './tagger.js';
+ import ipsum from '../../common/mocks/ipsum.js'
+ import RecDef from '../../common/RecDef.js';
+ import {onMount} from 'svelte'
+
+ onMount(
+     ()=>{
+         tagger.tagElement(ref,'text')
+     }
+ );
+
+ let ref;
+</script>
+<div>
+
+    <h2>Some content because content is rarely at the top of the page</h2>
+    <div><p bind:this={ref}>Initial Tag to Play with</p></div>
+    <h3>
+        Tag selection
+    </h3>
+    {#each RecDef.importProps as prop}
+        <button
+            on:click={()=>tagger.markupAndGetSelection(prop.name)}>
+            {prop.label}
+        </button>
+        {#if prop.hasDetail}
+            <button
+                on:click={()=>tagger.markupAndGetSelection(prop.name,'foo boo')}>
+                {prop.label} (w/ text)
+            </button>
+        {/if}
+    {/each}
+    {@html ipsum.generateParagraphs(5)}
+</div>
