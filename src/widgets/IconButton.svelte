2c07a419dde6525dd5e5542dcd4f3fa3078545a4
Tom Hinkle <tmhinkle@gmail.com>
2020-05-20 01:44:35 -0400

    iconbutton refactor

--- src/widgets/IconButton.svelte
+++ src/widgets/IconButton.svelte
@@ -1,15 +1,18 @@
 <script>
  let b;
  export let inverse=false;
+ export let icon
+ export let bare
  export function focus () {
      b.focus()
  }
 </script>
 
-<button class:inverse class="icon" on:click bind:this={b}>
+<button class:bare class:inverse class="icon" on:click bind:this={b}>
+    <slot>
+    </slot>
     <i  class="material-icons">
-        <slot>
-        </slot>
+        {icon}
     </i>
 </button>
 <style>
@@ -17,12 +20,20 @@
      background-color: #232323;
      color: white;
  }
+ button {
+     display: inline-flex;
+     justify-content: center;
+     align-content: center;
+ }
  button.icon {
-     border: none;
-     background-color: transparent;
      border-radius: 10px;
      transition: all 0.8s;
  }
+ button.bare {
+     border: none;
+     background-color: transparent;
+ }
+
  button.icon:hover {
      background-color: #7474f4;
      color: white;
