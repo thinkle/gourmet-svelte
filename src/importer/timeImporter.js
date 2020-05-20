c48876707adb84a93870988000bf620f46fa2f82
Tom Hinkle <tmhinkle@gmail.com>
2020-05-20 01:42:46 -0400

    improve texthandling

--- src/importer/timeImporter.js
+++ src/importer/timeImporter.js
@@ -1,38 +1,42 @@
 import {parse, end, toSeconds, pattern} from 'iso8601-duration';
 import Times from '../utils/times.js';
 import {parseAmount} from '../utils/numbers.js';
+import {cleanupWhitespace,titleCase} from '../utils/textUtils.js';
 
-function handleTime (chunk, context, recipe) {
+export function handleTime (chunk, context, recipe) {
+    console.log('handleTime!',chunk)
     let amount;
+    if (!amount) {
+        amount = parseAmount(chunk.text);
+        amount.unit = Times.getTimeUnit(chunk.text);
+        amount.seconds = amount.amount * amount.unit;
+        if (amount.pretext) {
+            amount.name = titleCase(cleanupWhitespace(amount.pretext))
+        }
+        else {
+            amount.name = 'Time'
+        }
+    }
+    amount.text = cleanupWhitespace(chunk.text)
     if (chunk.html) {
+        console.log('parse...');
         let duration
         // Try parsing time...
         let doc = new DOMParser().parseFromString(chunk.html,'text/html');
         let candidate = doc.querySelector('*[datetime]')
         if (candidate) {
-            let elWithDatetime = document.querySelector('*[datetime]')
-            if (elWithDatetime) {
-                let duration = toSeconds(parse(elWithDatetime['dateTime']));
-            }
-            amount = {
-                seconds : duration,
-                iso8601 : elWithDatetime['datetime']
-            }
+            console.log('found candidate in DOM...');
+            amount.seconds = toSeconds(parse(candidate['dateTime']));
+            amount.iso8601 = candidate['dateTime']
         }
     }
-    if (!amount) {
-        let amount = parseAmount(chunk.text);
-        amount.unit = Times.getTimeUnit(chunk.text);
-        amount.seconds = amount.amount * amount.unit;
-        if (amount.pretext) {
-            amount.label = titleCase(cleanupWhitespace(amount.pretext))
-        }
-        else {
-            amount.label = 'Time'
-        }
+    if (chunk.detail) {
+        amount.name = chunk.detail
     }
-    
-    amount.text = cleanupWhitespace(chunk.text)
+    console.log('Adding time: ',amount);
+    if (!recipe.times) {
+        recipe.times = []
+    }
     recipe.times.push(amount);
 }
 
