196258773fa6a3520b36c4a9645b5b4e5f0c02b4
Tom Hinkle <tmhinkle@gmail.com>
2020-05-19 20:11:11 -0400

    show/hide rec buttons

--- src/extension/web/ParseStatus.svelte
+++ src/extension/web/ParseStatus.svelte
@@ -1,11 +1,13 @@
 <script>
  import {onMount} from 'svelte';
+ import {parseData} from '../../importer/importer.js';
+ import Recipe from '../../webapp/recDisplay/Recipe.svelte'
  import {listenToExtension} from '../messaging/polling.js'
  let statusPromise;
  let pollAgain = true;
  let parsed
  let updateCount = 0;
-
+ 
  import RecDef from '../../common/RecDef.js';
  let poller;
  onMount(
@@ -24,48 +26,61 @@
          }
      }
  );
- 
+
+ let recipe
  let tagCount = 0;
  let tags = {}
  let tagNames = [];
+
  function updateParsed () {
      tags = {}
-     tagCount = 0
-     let items = Object.values(parsed);
-     for (let tag of items) {
-         if (tag.tag) {
-             if (!tags[tag.tag]) {
-                 tags[tag.tag] = 1;
-             }
-             else {
-                 tags[tag.tag] += 1;
-             }
-             tagCount += 1;
-         }
-     }
- }
- 
-let show=true;
+            tagCount = 0
+            let items = Object.values(parsed);
+            for (let tag of items) {
+                if (tag.tag) {
+                    if (!tags[tag.tag]) {
+                        tags[tag.tag] = 1;
+                    }
+                    else {
+                        tags[tag.tag] += 1;
+                    }
+                    tagCount += 1;
+                }
+            }
+            recipe = parseData(parsed)
+            
+            }
+         
+         let show=true;
+     let showRec = false;
 </script>
 <div>
     <!-- fix me: import  -->
     {#if poller}Connected to extension...{/if}
     <p>{updateCount} responses</p>
     <p>{tagCount} items tagged</p>
-    <ul>
-    {#if parsed}
-    {#each RecDef.importProps as prop}
-        {#if tags[prop.name]}
-            <li>{prop.label} : {tags[prop.name]}</li>
+    {#if recipe}
+        {#if showRec}
+            <button on:click={()=>showRec=false}>Hide Rec</button>
+            <Recipe editable={false} bind:rec={recipe}/>
+            Recipe: {JSON.stringify(recipe)}
+        {:else}
+            <button on:click={()=>showRec=true}>Show Rec</button>
         {/if}
-    {/each}
     {/if}
-
+    {#if parsed}
+        <ul>
+            {#each RecDef.importProps as prop}
+                {#if tags[prop.name]}
+                    <li>{prop.label} : {tags[prop.name]}</li>
+                {/if}
+            {/each}
+        </ul>
+    {/if}
     {#if show}
         <button on:click={()=>show=false}>Hide</button>
         {JSON.stringify(parsed)}
     {:else}
         <button on:click={()=>show=true}>Show JSON</button>
     {/if}
-    </ul>
 </div>
