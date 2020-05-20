d421c268179e1168a84f329c21bfc7ba0a3c8864
Tom Hinkle <tmhinkle@gmail.com>
2020-05-20 16:07:22 -0400

    Magic time parsing

--- src/utils/times.js
+++ src/utils/times.js
@@ -1,10 +1,13 @@
+import {floatToFrac,numMatchString,parseAmount} from './numbers.js';
+import {reToString} from './regExpUtil.js';
+
 let M = 60;
 let H = 60*60;
 let D = H * 24;
 let W = D * 7;
 let MNTH = D * 31
 let YR = D * 365;
-import {floatToFrac} from './numbers.js';
+
 
 
 function f2 (n) {
@@ -16,30 +19,143 @@ function f (n) {
 
 const timeWords = [
     {matcher:/\b(sec(ond)?s?)\b/i,
+     name:'second',
      value:1},
     {matcher:/\b(min(ute)?s?)\b/i,
+     name:'minute',
      value:M},
     {matcher:/\b(h(ou)?rs?)\b/i,
+     name:'hour',
      value:H},
     {matcher:/\b(da?ys?)\b/i,
+     name:'day',
      value:D},
     {matcher:/\b(we+ks?)\b/i,
+     name:'week',
      value:W},
     {matcher:/\b(mo?n?ths)\b/i,
+     name:'month',
      value:MNTH},
     {matcher:/\b(y(ea)?rs?)\b/i,
+     name:'year',
      value:YR}
 ]
 
-export default {
+//// Built this but then used unit extraction instead, so this is probably useless
+//// But I'm keeping the code around for good measure :)
+// Named groups for extracting values out of a string -- assumes values are ordered biggest to smallest units.
+//const timeValueMatcher = /(\b(?<year>\d)\s*(y(ea)?rs?)\b)?[,& and\s]*((?<month>\d)\s*(\b(mo?n?ths)\b))?[,& and\s]*((?<week>\d)\s*(\b(we+ks?)\b))?[,& and\s]*((?<day>\d)\s*(\b(da?ys?)\b))?[,& and\s]*((?<hour>\d)\s*(\b(h(ou)?rs?)\b))?[,& and\s]*((?<minute>\d)\s*(\b(min(ute)?s?)\b))?[,& and\s]*((?<second>\d)\s*(\b(sec(ond)?s?)\b))?/gi;
+// Next up, we have a matcher for IDENTIFYING strings - no named groups are possible with all the "or" statements we need, but it's still pretty cool
+// function buildTimeValueMatcher () {
+//     let regexp = '\\b'
+//     for (let i=timeWords.length-1; i > -1; i--) {
+//         let unit = timeWords[i]
+//         let matcher = reToString(unit.matcher);
+//         regexp += `(?<${unit.name}>${matcher})?`
+//         if (i > 0) {
+//             regexp += '(\\s*(and|\\W)?\\s*)?'
+//         }
+//     }
+//     return new RegExp(regexp,'ig');
+// }
 
-    getTimeUnit (s) {
-        for (let matcher of timeWords) {
+//const timeValueMatcher = buildTimeValueMatcher();
+
+function buildAtLeastOneTimeMatcher () {
+    let regexParts = []         // 
+    //for (let i=(timeWords.length-1); i > -1; i--) {
+    //let requiredUnit = timeWords[i];
+    for (let requiredUnit of timeWords) {
+        let regexPart = '('
+        let stopAt = timeWords.indexOf(requiredUnit);
+        for (let i=(timeWords.length-1); i >= stopAt; i--) {
+            let unit = timeWords[i];
+            //for (let unit of timeWords) {
+            let unitMatcher = '\\d+\\s*'+reToString(unit.matcher).replace(/\\b/g,'');
+            if (unit==requiredUnit) {
+                regexPart += '('+unitMatcher+')'
+            }
+            else {
+                regexPart += `(${unitMatcher})?`
+            }
+            if (i > stopAt) {
+                regexPart += '(\\s*(and|\\W)?\\s*)'
+            }
+        }
+        regexPart += ')'
+        regexParts.push(regexPart)
+    }
+    return new RegExp(regexParts.join('|'),'ig')
+}
+
+const timeMatcher = buildAtLeastOneTimeMatcher();
+const fancyTimeMatcher = new RegExp(reToString(timeMatcher).replace('\\d+',numMatchString),'ig');
+//const fancyTimeValueMatcher = new RegExp(reToString(timeValueMatcher).replace('\\d',numMatchString),'gi');
+
+export function getTimeUnit (s) {
+        for (let i=timeWords.length-1; i>-1; i--) {
+            let matcher = timeWords[i];
             if (s.match(matcher.matcher)) {
                 return matcher.value;
             }
         }
-    },
+}
+
+export function getSecondsFromString (s) {
+    let stringToParse = s
+    let amounts = []
+    while (stringToParse) {
+        let amount = parseAmount(stringToParse);
+        if (amount.posttext && amount.posttext != stringToParse) {
+            let timeUnit = getTimeUnit(amount.posttext)
+            if (amount.amount) {
+                amounts.push(
+                    {
+                        amount:amount.amount,
+                        unit:timeUnit
+                    }
+                )
+                stringToParse = amount.posttext
+            }
+            else {
+                stringToParse = '';
+            }
+        } else {
+            stringToParse = '';
+        }
+    }
+    let total = 0;
+    for (let t of amounts) {
+        total += t.amount * t.unit
+    }
+    return total
+}
+
+export function parseTimes (s) {
+    return s.replace(
+        timeMatcher,
+        (matchString)=>{
+            let seconds = getSecondsFromString(matchString)
+            // fix whitespace!
+            let {leading,content,trailing} = matchString.match(
+                    /^(?<leading>\s*)(?<content>.+?)(?<trailing>\s*)$/
+            ).groups;
+            return `${leading}<duration seconds=${seconds}>${content}</duration>${trailing}`
+        }
+    );
+}
+
+
+
+export default {
+    
+    timeMatcher,
+    //timeValueMatcher,
+    fancyTimeMatcher,
+    //fancyTimeValueMatcher,
+    getTimeUnit,
+    getSecondsFromString,
+    parseTimes,
     
     getTimeLabel (s) {
         if (s < 0) {
