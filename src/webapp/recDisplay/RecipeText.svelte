c55f31c11512180ef28b3036186d7c2afe0e15b8
Tom Hinkle <tmhinkle@gmail.com>
2020-05-20 16:08:09 -0400

    ++text w/ magic timers
    Just the start of the magic :_

---
+++ src/webapp/recDisplay/RecipeText.svelte
@@ -0,0 +1,21 @@
+<script>
+ import HtmlBlock from '../../widgets/HtmlBlock.svelte';
+ import TimeLink from '../../widgets/TimeLink.svelte';
+ export let value;
+ export let header=''
+ export let text=''
+ $: if (value) header = value.header
+ $: if (value) text = value.body
+ import {parseTimes} from '../../utils/times.js';
+ $: text = parseTimes(text);
+
+ let markupToComponentMap = {
+     DURATION : TimeLink,
+ }
+
+</script>
+<div>
+    <h2>{header}</h2>
+    <HtmlBlock markupToComponentMap={markupToComponentMap} content={text}/>    
+</div>
+
