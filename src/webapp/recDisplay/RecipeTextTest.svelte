8c96d50c92ae5e341bacf758adf80a3ab540c83b
Tom Hinkle <tmhinkle@gmail.com>
2020-05-20 16:08:12 -0400

    ++text w/ magic timers
    Just the start of the magic :_

---
+++ src/webapp/recDisplay/RecipeTextTest.svelte
@@ -0,0 +1,29 @@
+<script>
+ import Tester from '../../widgets/Tester.svelte';
+ import RecipeText from './RecipeText.svelte';
+ let hidden=true;
+ export let initialShow
+ let value = {
+     header : 'Instructions',
+     body :` How about a 5 second timer for testing?<br>
+<p>These are instructions. Bake for 30 minutes at 350 degrees.</p>
+<p>Are some of them getting lost?</p>
+<p>Then cook for 25 minutes</p>
+     `,
+     foo:'bar'
+ }
+ 
+ 
+</script>
+
+
+<Tester name="Recipe Text" initialShow={initialShow} >
+    <RecipeText value={value} />
+    <button
+        on:click={()=>hidden=!hidden}
+    >Toggle</button>
+    {#if !hidden}
+        <RecipeText value={value} />
+    {/if}
+
+</Tester>
