1e63957e716e5b1ca2c841503b18c039262774d8
Tom Hinkle <tmhinkle@gmail.com>
2020-05-21 17:29:36 -0400

    various fixes now that we're testing rigorously :)

--- src/data/functions/mongoConnect.js
+++ src/data/functions/mongoConnect.js
@@ -25,16 +25,14 @@ export async function insertMany (collection, docs) {
     const {db,client} = await loadDB(DB)
     let result = await db.collection(collection)
         .insertMany(docs)
-    return result;
+    return result.ops;
 }
 
 export async function insertOne (collection, doc) {
     const {db,client} = await loadDB(DB)
     let result = await db.collection(collection)
         .insertOne(doc)
-    let resultDoc = {...doc,
-                     _id : result.insertedId}
-    return resultDoc;
+    return result.ops[0];
 }
 
 export async function getOne (collection, query) {
@@ -44,36 +42,51 @@ export async function getOne (collection, query) {
     return result;
 }
 
+export async function replaceOne (collection, query, document) {
+    const {db,client} = await loadDB(DB)
+    let result = await db.collection(collection)
+        .replaceOne(query,document);
+    return result.ops[0];
+}
+
+
 export async function updateOne (collection, query, values) {
     const {db,client} = await loadDB(DB)
     let result = await db.collection(collection)
         .updateOne(query,values);
-    return result;
+    return result.ops[0];
+}
+
+export async function deleteOne (collection, query) {
+    const {db,client} = await loadDB(DB)
+    let result = await db.collection(collection)
+        .deleteOne(query);
+    return result.deletedCount
 }
 
 export async function queryCollection (collection, query, {fields, limit=100,page=0}={}) {
     const {db,client} = await loadDB(DB)
-    console.log('query',collection,'for',query)
+    //console.log('query',collection,'for',query)
     if (Array.isArray(fields)) {
         let _fields = {}
         fields.forEach((f)=>_fields[f]=1)
         fields = _fields
     }
-    console.log(query,fields);
+    //console.log(query,fields);
     let cursor = await db.collection(collection)
         .find(query);
     let count = await cursor.count()
-    console.log("COUNT",count,'limit',Number(limit))
+    //console.log("COUNT",count,'limit',Number(limit))
     if (page) {
         cursor.skip(page);
     }
     cursor.limit(Number(limit))
     if (fields) {
-        console.log('projecting to ',fields);
+        //console.log('projecting to ',fields);
         cursor = cursor.project(fields)
     }
     let result = await cursor.toArray()
-    console.log('Page:',result.length + page);
+    //console.log('Page:',result.length + page);
     return { // FIXME: handle pagination
         count:count,
         result:result,
@@ -90,10 +103,10 @@ async function adminTest () {
 export async function runTest () {
     return await insertOne('recipes',);
     const {db,client} = await loadDB('recipes')
-    console.log('Connected with more elegant syntax');
+    //console.log('Connected with more elegant syntax');
     let doc = await db.collection('test')
         .insertOne({greeting:'hello',audience:'world'})
-    console.log('Created doc:',doc);
+    //console.log('Created doc:',doc);
     return {
         doc,
         status : 'awesome sauce'
@@ -106,12 +119,12 @@ function runTestOrig () {
     const client = new MongoClient(url, { useNewUrlParser: true });
         client.connect(err => {
             if (err) {
-                console.log('Error?',err);
+                //console.log('Error?',err);
                 reject(err)
             }
             const collection = client.db("test").collection("devices");
             // perform actions on the collection object
-            console.log('I connected');
+            //console.log('I connected');
             client.close();
             resolve({status:'Success!'})
         });
