0571aaff4887720924b45755cf28aca3b6083beb
Tom Hinkle <tmhinkle@gmail.com>
2020-05-22 09:47:43 -0400

    Throw more and more useful errors

--- src/data/functions/recipeFunctions.js
+++ src/data/functions/recipeFunctions.js
@@ -1,31 +1,28 @@
-import {insertOne,insertMany,queryCollection,getOne,replaceOne,deleteOne} from './mongoConnect.js';
-import {validateRec}  from '../validate.js';
+import {require} from '../../utils/requireParams.js';
+import {jsonConcisify} from '../../utils/textUtils.js';
+import {getLastResult,insertOne,insertMany,queryCollection,getOne,replaceOne,deleteOne} from './mongoConnect.js';
+import {prepRecRemote}  from '../validate.js';
+
 
 export default {
     async addRecipe (event, context, user, params) {
         let {recipe} = params
-        validateRec(recipe);
-        recipe.user = user.email;
+        prepRecRemote(recipe,user);
+        console.log('Add recipe',recipe.title,JSON.stringify(recipe.owner))
         let result = await insertOne('recipes',
-                                     recipe);
+                                 recipe,recipe);
         return result;
     },
 
     async updateRecipe (event,context,user,params) {
+        require(['recipe._id'],params)
         let {recipe} = params;
-        if (!recipe.user) {
-            recipe.user = user.email;
-        } else {
-            if (recipe.user !== user.email) {
-                throw Error(
-                    `Attempted to update recipe you do not own: owner is ${recipe.user} but logged in user is ${user.email}`
-                )
-            }
-        }
+        prepRecRemote(recipe,user);
+        console.log('Replace recipe',recipe.title,JSON.stringify(recipe.owner))
         let result = await replaceOne('recipes',
-                                     {_id:recipe._id},
+                                      {_id:recipe._id,
+                                       'owner.email':user.email},
                                       recipe);
-        
         return result;
     },
 
@@ -34,19 +31,37 @@ export default {
     },
     
     async getRecipe (event,context,user,params) {
+        require(['_id'],params)
         let {_id} = params;
-        let recipe = await getOne('recipes',{_id, user:user.email})
+        if (!_id) {
+            throw new Error(
+                `getRecipe called without required parameter _id. Called with ${JSON.stringify(params)} instead.`
+            );
+        }
+        let recipe = await getOne('recipes',{_id, 'owner.email':user.email})
         if (recipe) {
             return recipe
         }
         else {
-            throw Error('No recipe found');
+            // try grabbing one not for user/???
+            //let recipe = await getOne('recipes',{_id})
+            //throw Error(`No recipe found for user ${user.email}, _id ${_id}: Found this though: ${recipe} -last result was ${getLastResult()}`);
+            throw Error(`No recipe found for user ${user.email}, _id ${_id}. Last DB result was ${getLastResult()}`);
         }
     },
 
     async getRecipes (event,context,user,params) {
         //console.log('getRecipes got params: ',JSON.stringify(params));
         let {page,query,fields,limit} = params;
+        // Enforce user only searches own recipes!
+        if (!query) {
+            query = {}
+        }
+        if (query['owner.email']) {
+                console.log('Warning: we had another email query???');
+                console.log('Quashing it: it was',query);
+        }
+        query['owner.email'] = user.email;
         if (!limit) {
             limit = 100;
         }
@@ -60,7 +75,16 @@ export default {
     },
 
     async deleteRecipe (event, context, user, params) {
-        let {recipe} = params
-        return await deleteOne('recipes',{_id:recipe._id,user:user.email});
+        require(['_id'],params)
+        const {_id} = params
+        let count = await deleteOne('recipes',{_id:_id,'owner.email':user.email});
+        if (count >= 1) {
+            return count;
+        }
+        else {
+            throw Error(
+                `No recipe found to delete. Request ${jsonConcisify(params)}. Result: ${getLastResult()}`
+            )
+        }
     }
 }
