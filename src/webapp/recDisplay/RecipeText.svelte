be52ec1d3f8d51cce885750e05a59c37738cf2e8
Tom Hinkle <tmhinkle@gmail.com>
2020-05-25 09:28:02 -0400

    fonts

--- src/webapp/recDisplay/RecipeText.svelte
+++ src/webapp/recDisplay/RecipeText.svelte
@@ -6,16 +6,34 @@
  export let text=''
  $: if (value) header = value.header
  $: if (value) text = value.body
+ //text = parseTimes(text);
+ 
  import {parseTimes} from '../../utils/times.js';
- $: text = parseTimes(text);
-
+ $: {
+     //console.log('Parsing text: ',text);
+     text = parseTimes(text);
+     //console.log('Success:',text)
+ }
+ 
  let markupToComponentMap = {
      DURATION : TimeLink,
  }
+
 
 </script>
 <div>
     <h2>{header}</h2>
     <HtmlBlock markupToComponentMap={markupToComponentMap} content={text}/>    
 </div>
-
+<style>
+ h2 {
+     font-family : var(--recipeHeadFont);
+     font-weight: bold;
+     font-size: 1.5rem;
+ }
+ div {
+     font-family: var(--recipeFont);
+     font-size : 1rem;
+     line-height: 1.4;
+ }
+</style>
