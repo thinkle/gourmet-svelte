2bc000ef74be1e0f463769bb8d35a5bfbdb20b4d
Tom Hinkle <tmhinkle@gmail.com>
2020-05-26 08:00:17 -0400

    ++Styling. openOne method to allow opening from the outside. Comments and formatting.

--- src/webapp/recDisplay/OpenRecipes.svelte
+++ src/webapp/recDisplay/OpenRecipes.svelte
@@ -1,7 +1,15 @@
 <script>
+ import {flip} from 'svelte/animate';
+ import { quintOut } from 'svelte/easing';
  import Recipe from './Recipe.svelte'
+ import IconButton from '../../widgets/IconButton.svelte';
  import {openLocalRecipes,localRecipes,recipeState} from '../../stores/recipeStores.js';
-
+ export function open (id) {
+     hide = false;
+     if (id!==undefined) {
+         activeRecipeId = id;
+     }
+ }
  function getTabTitle (id) {
      return $localRecipes[id].title && $localRecipes[id].title.substr(0,30) || '???'; // fixme
  }
@@ -9,13 +17,28 @@
  let activeRecipeId
  let hide = false;
 
- $: if (!activeRecipeId && $openLocalRecipes.length) {activeRecipeId = $openLocalRecipes[0]}
+ function openOne () {
+     activeRecipeId = $openLocalRecipes[0]
+     console.log('Got open recs',$openLocalRecipes);
+ }
+
+ $: {if (!activeRecipeId && $openLocalRecipes.length && $openLocalRecipes[0]) {
+     openOne()
+    }}
+            console.log('Got recs',$openLocalRecipes);
 
  let showCloseModalFor
+
  function closeRec (id,confirmed=false) {
+     
      showCloseModalFor = undefined;
-     if (!$recipeState[id].edited||confirmed) {
+     if (!$recipeState[id]||!$recipeState[id].edited||confirmed) {
+         let index = $openLocalRecipes.indexOf(id);
          localRecipes.close(id);
+         if (index > 0) {
+             index = index + 1;
+         }
+         activeRecipeId = $openLocalRecipes[index]; // change ID
      } else {
          console.log('Set closed modal!');
          showCloseModalFor = id;
@@ -24,38 +47,57 @@
 
 </script>
 <div>
-    {#if $openLocalRecipes.length > 0}
-    <div class="tabbox" class:hidden={hide}>
-        <div class="tabs">
-            {#each $openLocalRecipes as id}
-                <span class:active={activeRecipeId==id} on:click={()=>activeRecipeId=id}>{getTabTitle(id)}
-                    <button class="icon" on:click={()=>closeRec(id)}><i class="material-icons" >close</i></button>
-                </span>
-            {/each}
-            <button on:click="{()=>hide=!hide}">
-                {#if hide}
-                    Max
-                {:else}
-                    Min
-                {/if}
-            </button>
-
-        </div>
-        <div class="content">
-            <Recipe onChange={(rec)=>$localRecipes[activeRecipeId]=rec} rec={$localRecipes[activeRecipeId]}/>
-            {#if showCloseModalFor!==undefined}
-                <div class="modal" >
-                {$localRecipes[showCloseModalFor] && $localRecipes[showCloseModalFor].title} has changed, close and lose changes?
-                <button on:click={()=>closeRec(showCloseModalFor,true)}>
-                    Close anyway, I don't care about my changes
-                </button>
-                <button on:click={()=>showCloseModalFor=undefined}>
-                    Oh wait, nevermind, I'm so sorry!
-                </button>
+    {#if ($openLocalRecipes.length > 0)}
+        <div class="tabbox" class:hidden={hide}>
+            <div class="tabs">
+                {#each $openLocalRecipes as id (id)}
+                    <span
+                        animate:flip={{delay:100,duration:250,easing:quintOut}}
+                        class:active={activeRecipeId==id} on:click={()=>activeRecipeId=id}>
+                        <div class='close'><IconButton bare={true} small={true} on:click={()=>{console.log('close fired',id);closeRec(id)}} icon='close'/></div>
+                        {getTabTitle(id)}
+                        
+                    </span>
+                {/each}
+                <div class='toggle'>
+                    <IconButton on:click="{()=>hide=!hide}"
+                                icon={hide&&"expand_more"||"expand_less"}
+                    >
+                        {#if hide}
+                            Show Recipes
+                        {:else}
+                            Hide Recipes
+                        {/if}
+                    </IconButton>
                 </div>
-            {/if}
-        </div>
-    </div>
+            </div> <!-- close tabs -->
+
+            <div class="content">
+                <!--  $openLocalRecipes.indexOf(activeRecipeId)>-1 &&  ?? -->
+                {#if $localRecipes[activeRecipeId]}
+                    <Recipe onChange={(rec)=>{
+                                     console.log('onChange fired!');
+                                     $localRecipes[rec.id]=rec
+                                     }}} rec={$localRecipes[activeRecipeId]}/>
+                {:else}
+                    {$openLocalRecipes.length>0 && openOne()}                    
+                {/if}
+                <!-- Are you sure you want to close modal... -->
+                {#if showCloseModalFor!==undefined}
+                    <div class="modal" >
+                        {$localRecipes[showCloseModalFor] && $localRecipes[showCloseModalFor].title} has changed, close and lose changes?
+                        <button on:click={()=>closeRec(showCloseModalFor,true)}>
+                            Close anyway, I don't care about my changes
+                        </button>
+                        <button on:click={()=>showCloseModalFor=undefined}>
+                            Oh wait, nevermind, I'm so sorry!
+                        </button>
+                    </div>
+                {/if}
+            </div> <!-- close content -->
+
+        </div> <!-- close tabbox -->
+
     {/if}
 </div>
 <style>
@@ -90,22 +132,48 @@
  .tabbox .content {
      height: 90vh;
      opacity: 1;
+     width: calc(100vw - 20px);
+     background-color: white;
+ }
+
+ .tabbox {
+     position: fixed;
+     top: 5vh;
+     left: 10px;
+ }
+
+ .tabs {
+     display: flex;
+     flex-direction: row;
+     background-color: white;
+ }
+
+ .hidden .tabs {
+     background-color: transparent;
  }
 
  .tabs span {
-     display: inline-block;
      padding: 0.5em;
-     box-shadow: 3px 3px #887;
+     border-bottom : 1px solid var(--light-underline);
+     margin-right: 3px;
+     font-weight: 300; /* light */
  }
 
  .tabs span:hover {
-     background-color : #777;
+     border-bottom: 3px solid var(--medium-underline);
+     background-color: var(--tabhover-bg);
+     color: var(--tabhover-fg);
+     font-weight: 500;
+ }
+ .toggle {
+     margin-left: auto;
  }
 
- .active {
-     text-decoration: underline;
-     background-color: #999;
-     color: #ddd;
+ .tabs .active {
+     border-bottom: 3px solid var(--heavy-underline);
+     font-weight: 500;
  }
- 
+ .tabs span .close {
+     float : right;
+ }
 </style>
