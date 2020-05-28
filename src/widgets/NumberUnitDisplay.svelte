ef3198dbc75eb34ca2ef16d16dfe312b121de9d9
Tom Hinkle <tmhinkle@gmail.com>
2020-05-28 02:02:21 -0400

    don't fail w/ no value

--- src/widgets/NumberUnitDisplay.svelte
+++ src/widgets/NumberUnitDisplay.svelte
@@ -7,19 +7,24 @@
  import {getContext} from 'svelte';
  let multiplier = getContext('multiplier')
 </script>
-{#if mode=='table'}
-    <td class="amount" class:multiplied={$multiplier!=1}>
-        {formatAmount(value,{multiplier:$multiplier})}
-    </td>
-    &nbsp;
-    <td class="unit" >{value.unit||''}</td>
+{#if value}
+    {#if mode=='table'}
+        <td class="amount" class:multiplied={$multiplier!=1}>
+            {formatAmount(value,{multiplier:$multiplier})}
+        </td>
+        &nbsp;
+        <td class="unit" >{value.unit||''}</td>
 
-{:else}
-<span class="amount" class:multiplied={$multiplier!=1}>
-    {formatAmount(value,{multiplier:$multiplier})}
-</span>
-&nbsp;
-<span class="unit" >{value.unit||''}</span>
+    {:else}
+        <span class="amount" class:multiplied={$multiplier!=1}>
+            {formatAmount(value,{multiplier:$multiplier})}
+        </span>
+        &nbsp;
+        <span class="unit" >{value.unit||''}</span>
+    {/if}
+{:else if (mode=='table')}
+    <td class='amount'></td>
+    <td class='unit'></td>
 {/if}
 
 <style>
