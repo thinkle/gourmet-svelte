e3cb1add402374126c239a088d65816cca4ebb0c
Tom Hinkle <tmhinkle@gmail.com>
2020-05-28 06:49:14 -0400

    Widget for highlighting an ingredient in recipe text

---
+++ src/webapp/recDisplay/IngHighlight.svelte
@@ -0,0 +1,51 @@
+<script>
+ export let target
+ import {getContext} from 'svelte';
+
+ let highlightedIngredient = getContext('highlightedIngredient')
+
+ function toggleHighlight () {
+     if ($highlightedIngredient.highlighted != target) {
+         $highlightedIngredient.highlighted=target
+     }
+     else {
+         $highlightedIngredient.highlighted=undefined
+     }
+ }
+ function hoverOn () {
+     $highlightedIngredient.hover=target
+ }
+ function hoverOff () {
+     $highlightedIngredient.hover=undefined
+ }
+ 
+
+
+</script>
+
+<span
+    class:hover={target==$highlightedIngredient.hover} class:highlight={target==$highlightedIngredient.highlighted}
+    on:click={toggleHighlight}
+    on:mouseover={hoverOn}
+    on:mouseleave={hoverOff}
+>
+    <slot/>
+</span>
+
+<style>
+
+ .hover {
+     background-color: var(--note-light-bg);
+     color: var(--note-light-fg);
+ }
+ 
+ .highlight {
+     background-color: var(--note-bg);
+     color: var(--note-fg);
+     
+ }
+ span {
+     font-weight: 900;
+     color: #444;
+ }
+</style>
