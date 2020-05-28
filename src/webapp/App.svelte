01b30dac3c4a3dc36087ab257de65b4d3f749332
Tom Hinkle <tmhinkle@gmail.com>
2020-05-28 01:57:24 -0400

    Rework landing/login routing

--- src/webapp/App.svelte
+++ src/webapp/App.svelte
@@ -1,8 +1,10 @@
 <script>
  import router from 'page';
  import Landing from './Landing.svelte';
+ import Admin from './Admin.svelte';
  import Recipe from './recDisplay/Recipe.svelte';
  import RecipeList from './recDisplay/RecipeList.svelte';
+ import SingleRecipe from './recDisplay/SingleRecipe.svelte';
  import Demo from './Demo.svelte';
  import Sidebar from '../extension/web/Sidebar.svelte';
  import {testRecs} from '../common/mocks/recipes.js'
@@ -11,7 +13,14 @@
  let name = 'Gourmet BUILD_TIME';
  let page;
  let params = {};
- router('/',()=>{page = Landing; params={}});
+ router('/',()=>{page = RecipeList; params={}});
+ router('/admin',()=>{page = Admin; params={}});
+ router('/rec/:id',(ctx)=>{
+     page = SingleRecipe
+     params = {
+         id:Number(ctx.params.id)
+     };
+ });
  router('/sidebar',()=>{page = Sidebar; params={}});
  router('/rl',()=>{page = RecipeList; params={}});
  router('/oneRec',()=>{page = Recipe; params={rec:testRecs.standard}});
@@ -36,7 +45,9 @@
 
 <div>
     <p>{name}</p>
-    <svelte:component this={page} {...params}/>
+    <Landing>
+        <svelte:component this={page} {...params}/>
+    </Landing>
 </div>
 <style>
  p {
@@ -47,10 +58,32 @@
      margin: auto;
      max-width: 1200px;
  }
+
  :root {
+     /* Colors */
      --grey : #727272;
      --black : #efefef;
      --white : #121212;
-     --font : Futura, sans-serif;
+     --light-bg : #fbf6ff;
+     --light-fg : #111122;
+     --medium-bg: #dadaff;
+     --medium-fg: #343434;
+     --focus-border: #2222df;
+     --light-underline : #aaa;
+     --medium-underline : #666;
+     --heavy-underline : #aaa;
+     --note-bg : #ffffad;
+     --note-fg : #333333;
+     --note-light-bg : #ffffdd;
+     --note-light-fg: #333;
+     /* Typography */
+     --recipeFont : Lora, Ubuntu, Cantarell, serif;
+     --recipeHeadFont : Lora,'Noto Sans',Ubuntu,Cantarell,-apple-system,sans-serif;
+     --brandFont : 'Gimlet Sans Web','Noto Sans',Ubuntu,Cantarell,-apple-system,sans-serif;
+     --uiFont : 'Noto Sans', Ubuntu, Cantarell, -apple-system, sans-serif;
+     --small : 0.8rem;
+     --xsmall : 0.6rem;
  }
- </style>
+
+ 
+</style>
