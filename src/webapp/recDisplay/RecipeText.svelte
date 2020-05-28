73b112a7d3199abf00c35d4e1d0a25be0c42c3b4
Tom Hinkle <tmhinkle@gmail.com>
2020-05-28 01:59:26 -0400

    mark-up ingredients so we can highlight them automatically

--- src/webapp/recDisplay/RecipeText.svelte
+++ src/webapp/recDisplay/RecipeText.svelte
@@ -1,29 +1,39 @@
 <script>
  import HtmlBlock from '../../widgets/HtmlBlock.svelte';
  import TimeLink from '../../widgets/TimeLink.svelte';
+ import IngHighlight from './IngHighlight.svelte';
  export let value;
  export let header=''
  export let text=''
  $: if (value) header = value.header
  $: if (value) text = value.body
+ import {getContext} from 'svelte'
+ let ingredientList = getContext('ingredientList');
  //text = parseTimes(text);
- 
  import {parseTimes} from '../../utils/times.js';
+ import {parseIngredients} from '../../utils/ingredientUtils.js';
+ let parsed
  $: {
-     //console.log('Parsing text: ',text);
-     text = parseTimes(text);
-     //console.log('Success:',text)
+     console.log('Parsing text: ',text);
+     if (text) {
+         parsed = parseIngredients(text,$ingredientList);
+         parsed = parseTimes(parsed);
+         console.log('Success:',parsed)
+ }
  }
  
  let markupToComponentMap = {
      DURATION : TimeLink,
+     ING: IngHighlight,
  }
 
 
 </script>
 <div>
     <h2>{header}</h2>
-    <HtmlBlock markupToComponentMap={markupToComponentMap} content={text}/>    
+    {#each [parsed] as _ (parsed)} <!-- Make it rerender when parsed changes -->
+        <HtmlBlock markupToComponentMap={markupToComponentMap} content={parsed}/>
+    {/each}
 </div>
 <style>
  h2 {
@@ -36,4 +46,10 @@
      font-size : 1rem;
      line-height: 1.4;
  }
+
+ div :global(h1),div :global(h2),div :global(h3),div :global(h4),div :global(h5),div :global(h6) {
+     margin-bottom: 3px;
+     border-bottom: 1px solid var(--light-underline);
+ }
+ 
 </style>
