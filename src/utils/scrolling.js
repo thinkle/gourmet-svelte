9ecb528dbab5296d4666247a210045fbb128aa7f
Tom Hinkle <tmhinkle@gmail.com>
2020-05-29 11:00:16 -0400

    ++Scroll-into-view utility function

---
+++ src/utils/scrolling.js
@@ -0,0 +1,25 @@
+import { tweened } from 'svelte/motion';
+import { cubicOut } from 'svelte/easing';
+
+import scrollparent from 'scrollparent'
+
+export function scrollIntoView (node) {
+    let parent = scrollparent(node);
+    let scrollTop = tweened(parent.scrollTop,{
+        duration : 1000,
+        easing : cubicOut
+    });
+    let unsubscribe = scrollTop.subscribe(
+        (v)=>parent.scrollTop=v
+    );
+    //parent.scrollTop = (node.offsetTop - parent.clientHeight/2); // middle of parent
+    scrollTop.set(node.offsetTop - parent.clientHeight/2) // middle of parent
+        .then(
+            unsubscribe
+        );
+}
+
+export function fillWithoutScrolling (node) {
+    let parent = scrollparent(node);
+    
+}
