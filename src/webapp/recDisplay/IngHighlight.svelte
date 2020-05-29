cf7f4140568ffea876b2686e2d712122e708fa66
Tom Hinkle <tmhinkle@gmail.com>
2020-05-29 11:01:16 -0400

    Scroll highlighted ingredient into view

--- src/webapp/recDisplay/IngHighlight.svelte
+++ src/webapp/recDisplay/IngHighlight.svelte
@@ -1,4 +1,5 @@
 <script>
+ import {scrollIntoView} from '../../utils/scrolling.js';
  export let target
  import {getContext} from 'svelte';
 
@@ -7,6 +8,7 @@
  function toggleHighlight () {
      if ($highlightedIngredient.highlighted != target) {
          $highlightedIngredient.highlighted=target
+         $highlightedIngredient.scrolledTextFor=target; // don't scroll after we click :)
      }
      else {
          $highlightedIngredient.highlighted=undefined
@@ -19,15 +21,37 @@
      $highlightedIngredient.hover=undefined
  }
  
+ // We might eventually care about de-registering, but it probably doesn't matter much.
+ // The UI "problem" not deregistering will create is just that ingredients in the ing-list
+ // will highlight after we've deleted some text with them. Not that big a deal I think.
+ $highlightedIngredient.active = [...$highlightedIngredient.active,target]
 
+ import {derived} from 'svelte/store'
+ let highlightedItem =  derived(highlightedIngredient,($highlightedIngredient)=>$highlightedIngredient.highlighted);
+
+ function scroll (node,highlighted) {
+     console.log('use scroll');
+
+     return {
+         update (highlighted) {
+             if (highlighted===target && $highlightedIngredient.scrolledTextFor!==target) {
+                 scrollIntoView(node);
+                 $highlightedIngredient.scrolledTextFor = target;
+             } 
+             
+         }
+     }
+ }
 
 </script>
 
 <span
-    class:hover={target==$highlightedIngredient.hover} class:highlight={target==$highlightedIngredient.highlighted}
-    on:click={toggleHighlight}
-    on:mouseover={hoverOn}
-    on:mouseleave={hoverOff}
+    class:hover="{target==$highlightedIngredient.hover}"
+    class:highlight="{target==$highlightedIngredient.highlighted}"
+    on:click="{toggleHighlight}"
+    on:mouseover="{hoverOn}"
+    on:mouseleave="{hoverOff}"
+    use:scroll="{$highlightedItem}"
 >
     <slot/>
 </span>
