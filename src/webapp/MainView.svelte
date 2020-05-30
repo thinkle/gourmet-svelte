b88e441bb386ce907211878f109978f6c804e309
Tom Hinkle <tmhinkle@gmail.com>
2020-05-30 11:29:31 -0400

    ++Make a separate main view widget so that we can re-use our recipe list elsewhere (like when picking linked recipes for ingredients)

---
+++ src/webapp/MainView.svelte
@@ -0,0 +1,86 @@
+<script>
+ import {registerBuild} from '../stores/debug.js'; registerBuild(BUILD_MS);
+ import RecipeList from './recDisplay/RecipeList.svelte';
+ import OpenRecipes from './recDisplay/OpenRecipes.svelte';
+ import Status from '../widgets/Status.svelte';
+ import IconButton from '../widgets/IconButton.svelte';
+ import {connected,
+        localRecipes,
+        openLocalRecipes,
+        recipeActions} from '../stores/recipeStores.js';
+ let opener;
+ let syncingPromise
+
+
+ // Fix me
+ import {storedRecipes} from '../stores/recipeStores.js';
+ import JsonDebug from '../widgets/JsonDebug.svelte';
+
+ import {getContext,onMount} from 'svelte';
+ import {writable} from 'svelte/store'
+ let hideOpenButton = writable(true);
+ $: {
+     toolbarItem && $openLocalRecipes.length>=0 && toolbarItem.show()
+ }
+ $: {
+     toolbarItem && $openLocalRecipes.length<=0 && toolbarItem.hide()
+ }
+
+ let toolbar = getContext('toolbar');
+ let toolbarItem;
+ onMount(
+     ()=>{
+         if (!toolbar) {return}
+         let {onUnmount,show,hide} = toolbar.addItem({
+             content : 'Open Recipes',
+             props : {
+                 icon : 'expand_more',
+             },
+             'onClick' : opener.open
+         });
+         toolbarItem = {
+             show,hide
+         }
+         return onUnmount
+     }
+ );
+ 
+</script>
+
+<div>
+    {#if $connected}
+        <br>
+        <button
+            on:click="{()=>recipeActions.createRecipe()}">
+            New Recipe
+        </button>
+    {/if}
+    <div>
+        <OpenRecipes bind:this={opener} recipes={open}/>
+    </div>    
+    <RecipeList
+        onRecipeClick="{
+                       (id)=>localRecipes.open(id).then(()=>opener.open(id))
+                       }"
+    />
+    <Status/>
+    <button on:click="{()=>syncingPromise=recipeActions.doSync()}">Sync with Server?</button>
+    <button on:click="{()=>syncingPromise=recipeActions.doSync(true)}">Small Sync</button>
+    {#if syncingPromise}
+        {#await syncingPromise}
+            Syncing...
+        {:then json}
+            Cool, done syncing! {JSON.stringify(json)}
+        {:catch error}
+            Failed :( {console.log(error)} {error}
+        {/await}
+    {/if}   
+</div>
+{#if DEV}
+<div>
+    Stored<JsonDebug data={$storedRecipes}/>
+    Local<JsonDebug data={$localRecipes}/>
+</div>
+{/if}
+<style>
+</style>
