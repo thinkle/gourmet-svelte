787a5e33afc717b6f2e04443215832fdbdf2a722
Tom Hinkle <tmhinkle@gmail.com>
2020-05-30 08:23:54 -0400

    ++SVG

--- src/widgets/widgetDemos.js
+++ src/widgets/widgetDemos.js
@@ -1,9 +1,12 @@
+import {registerBuild} from '../stores/debug.js'; registerBuild(BUILD_MS);
 import InputTests from './Inputs.demo.svelte';
 import TimerTests from './Timer.demo.svelte';
 import IngredientInput from './IngredientInput.demo.svelte';
+import SVG from './SVGs.demo.svelte';
 
  export default {
      'inputs':InputTests,
      'timers':TimerTests,
-     'ingredient':IngredientInput
+     'ingredient':IngredientInput,
+     'svg':SVG,
  }
