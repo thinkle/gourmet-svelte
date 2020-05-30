915a10c51e57a7ff4da037942c07a686114dc921
Tom Hinkle <tmhinkle@gmail.com>
2020-05-29 21:27:23 -0400

    ++multi-word search

--- src/data/dexieApi.js
+++ src/data/dexieApi.js
@@ -34,21 +34,84 @@ const dexieApi = {
         }
     },
 
+    searchWord (word) {
+        let q = dexieApi.db.recipes
+        q = q.where('words').startsWith(
+            word.toLowerCase()
+        ); // fix for multiple words...        
+        return q
+    },
+
+    async searchWords (words) {
+        let q = dexieApi.db.recipes
+        var ids = undefined;
+        for (let word of words) {
+            let subResults = dexieApi.searchWord(word);
+            let subIds = await subResults.primaryKeys()
+            if (!ids) {
+                ids = subIds;
+                console.log('DX: ',word,'=>',ids.length,'results');
+            } else {
+                ids = ids.filter(
+                    (id)=>subIds.indexOf(id)>-1
+                );
+                console.log('DX Down to ',ids.length,'results',word);
+                if (ids.length == 0) {
+                    return undefined
+                }
+            }
+        }
+        console.log('Got IDs',ids);
+        q = q.where(':id').anyOf(ids);
+        q.alreadyCounted = ids.length;
+        return q;
+    },
+
     async getRecipes({query,fields,limit,page}={}) {
         let q = dexieApi.db.recipes
-        if (query) {
-            if (query.fulltext) {
-                q = q.where('words').startsWith(
-                    query.fulltext.toLowerCase()
-                ); // fix for multiple words...
+        if (query.fulltext) {
+            if (query.fulltext.indexOf(' ')>-1) {
+                query.fulltext = query.fulltext.replace(/^\s+|\s+$/g,'')
+                q = await dexieApi.searchWords(
+                    query.fulltext.split(/\s+/)
+                )                
             }
+            else {
+                q = dexieApi.searchWord(query.fulltext);
+            }
+        }
+        if (!q) {
+            return {
+                result:[],
+                count:0,
+            }
+        }
+        let count
+        if (q.alreadyCounted) {
+            console.log('Already counted')
+            count = q.alreadyCounted
+        }
+        else if (q.clone) {
+            console.log('Clone count?');
+            count = await q.clone().count()
+        }
+        else {
+            console.log('Collection count');
+            count = await q.count();
         }
         if (page) {q = q.offset(page)}
         if (limit) {q = q.limit(limit)}
         let result = await q.toArray();
+        let previousPage = 0;
+        if (page && result.length) {
+            previousPage = page - result.length
+        }
         return {
             result,
-            page : result.length + page,
+            count,
+            prevPage : previousPage,
+            nextPage : result.length + (page||0),
+            currentPage : page||0,            
         }
         
     },
