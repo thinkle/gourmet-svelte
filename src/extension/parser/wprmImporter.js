74040c24bdbebecd30606091f050eea8f08fb514
Tom Hinkle <tmhinkle@gmail.com>
2020-05-30 20:19:16 -0400

    ++Parser for blog recipes we found...

---
+++ src/extension/parser/wprmImporter.js
@@ -0,0 +1,18 @@
+export const wprmSelector = 'wprm-recipe-container';
+
+export const importer = [
+    {selector:'.wprm-recipecontainer',
+     tag:'recipe'},
+    {selector:'.wprm-recipe-ingredient-group',
+     tag:'inggroup'},
+    {selector:'.wprm-recipe-ingredient',
+     tag:'ingredient'},
+    {selector:'.wprm-recipe-ingredient-amount',
+     tag:'amount'},
+    {selector:'.wprm-recipe-ingredient-name',
+     tag:'ingredientText'},
+    {selector:'.wprm-recipe-instructions-container',
+     tag:'text'},
+    {selector:'.wprm-recipe-time-container',
+     tag:'time'},
+]
