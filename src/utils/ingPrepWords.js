daa8f7eabf2be41bf58ee86070e21b54090bca93
Tom Hinkle <tmhinkle@gmail.com>
2020-05-30 20:18:17 -0400

    ++verbs!

--- src/utils/ingPrepWords.js
+++ src/utils/ingPrepWords.js
@@ -19,40 +19,58 @@ let adjs = [
 ];
 let verbs = [
     'bake',
+    'blanch',
     'boil',
     'chop',
+    'cool',
     'core',
+    'cube',
     'cut',
+    'defrost',
     'devein',
     'dice',
     'discard',
+    'flambe',
+    'flambé',
     'fry',
+    'grill',
+    'grind',
     'halve',
+    'heat',
     'hold',
     'keep',
+    'melt',
     'mince',
     'mix',
     'peel',
+    'poach',
     'process',
     'retain',
+    'rub',
     'saute',
     'sauté',
     'scrape',
     'serve',
     'shake',
     'shook',
+    'shred',
     'sift',
     'slice',
+    'soften',
     'sprinkle',
+    'squeeze',
+    'squirt',
     'stem',
+    'stir',
     'throw',
     'thrown',
     'toss',
+    'whisk'
 ];
 // we'll be grammatical profligates...
 for (let v of verbs) {
     words.push(v);
-    if ('ptsm'.indexOf(v.substr(v.length-1))>-1) {
+    if ('bdmpts'.indexOf(v.substr(v.length-1))>-1) {
         words.push(v+v.substr(v.length-1)+'ed');
         words.push(v+v.substr(v.length-1)+'ing');
     }
