9ac6eefcb5144847f6cd1c1b2154e6aeb74ef50f
Tom Hinkle <tmhinkle@gmail.com>
2020-06-01 15:52:55 -0400

    ++Code to check if recs have changed

---
+++ src/data/diff.js
@@ -0,0 +1,28 @@
+import RecDef from '../common/RecDef.js'
+
+export function diffRecs (r1, r2) {
+    let diffs = []
+    for (let p of [...RecDef.titleProps,RecDef.recProps,{name:'images',array:true},{name:'ingredients',array:true}]) {
+        if (!propIsEqual(r1[p.name],r2[p.name],p)) {
+            diffs.push(p,r1[p.name],r2[p.name])
+        }
+    }
+    if (diffs.length == 0) {
+        return false
+    } else {
+        return diffs
+    }
+}
+
+function propIsEqual (v1, v2, p) {
+    if (JSON.stringify(v1) === JSON.stringify(v2)) {
+        return true;
+    } else {
+        if (p.array) {
+            if ((!v1 || v1.length==0) && (!v2 || v2.length==0)) {
+                return true;
+            }
+        }
+    }
+    return false; // default
+}
