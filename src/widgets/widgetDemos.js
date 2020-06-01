a91e2c3a0cdcabbdd69eb9babda85add7c398894
Tom Hinkle <tmhinkle@gmail.com>
2020-05-31 21:25:25 -0400

    ++menu

--- src/widgets/widgetDemos.js
+++ src/widgets/widgetDemos.js
@@ -3,10 +3,12 @@ import InputTests from './Inputs.demo.svelte';
 import TimerTests from './Timer.demo.svelte';
 import IngredientInput from './IngredientInput.demo.svelte';
 import SVG from './SVGs.demo.svelte';
+import MenuDemo from './Menu.demo.svelte';
 
  export default {
      'inputs':InputTests,
      'timers':TimerTests,
      'ingredient':IngredientInput,
      'svg':SVG,
+     'menu':MenuDemo,
  }
