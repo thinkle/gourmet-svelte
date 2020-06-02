82a1d7d4fcecba37c8e75213501482fdeee4e7fc
Tom Hinkle <tmhinkle@gmail.com>
2020-06-02 00:16:48 -0400

    ++reorg of extension webapp code

---
+++ src/extension/web/SidebarRecipeViews.svelte
@@ -0,0 +1,55 @@
+<script>
+ export let recipe
+ 
+ import Recipe from '../../webapp/recDisplay/Recipe.svelte'
+ import {recipeActions,connected,storedRecipes} from '../../stores/recipeStores.js';
+ import IconButton from '../../widgets/IconButton.svelte';
+ let mode = 'full'
+
+ // promise
+ let saving;
+
+ function doSaveRecipe () {
+     saving = recipeActions.createRecipe(recipe);    
+ }
+
+</script>
+<div>
+    <span class:active="{mode=='full'}" on:click="{()=>{mode='full'}}">Whole Recipe</span>
+    <span class:active="{mode=='summary'}" on:click="{()=>{mode='summary'}}">Summary</span>
+    <span class="important" class:active="{mode=='save'}" on:click="{()=>{mode='save'}}">Save</span>
+</div>
+{#if mode=='full'}
+    <Recipe editable={false} rec={recipe}/>
+{:else if mode=='summary'}
+    <p>Sorry, this view will be awesome, but I haven't written it yet.</p>
+{:else}
+    Once you have saved your recipe, you can view and edit it to your heart's content.
+    If you would rather make changes <em>here</em>, enter the tagging view.
+    {#if $connected}<IconButton icon="save" on:click={doSaveRecipe}>Save Recipe to Collection</IconButton>{/if}
+    {#if saving}
+        {#await saving}
+            Creating recipe in database...
+        {:then recipe}
+            <a href={`${location.origin}/`} target="_BLANK">Open collection...</a>
+            <a href={`${location.origin}/rec/${recipe.id}`} target="_BLANK">Open recipe</a>
+        {:catch error}
+            Unable to create recipe {error}
+        {/await}
+    {/if}
+{/if}
+<style>
+ span {
+     display: inline-block;
+     padding : 5px;
+     font-weight: 500;
+     border-bottom: 1px solid var(--light-underline);
+ }
+ span.active {
+     font-weight: 900;
+     border-bottom: 3px solid var(--heavy-underline);
+ }
+ .important {
+     border: 1px solid var(--light-underline);
+ }
+</style>
