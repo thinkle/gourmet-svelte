14d3bee2852b44ced7b32d8535251286c1b3e50f
Tom Hinkle <tmhinkle@gmail.com>
2020-06-03 23:20:31 -0400

    Remove old polling listener

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,7 +1,7 @@
 import Metadata from '../common/RecDef.js';
 import {listen} from './parser/backgroundParser.js';
 import {launchTab} from './SidebarCommunication.js';
-import {listenForConnections} from './messaging/polling.js';
+//import {listenForConnections} from './messaging/polling.js';
 import ContextMenus from './ContextMenus.js';
 console.log('Extension last built at BUILD_TIME')
 listen();
@@ -10,4 +10,15 @@ chrome.browserAction.onClicked.addListener(launchTab);
 console.log('We are listening...');
 ContextMenus.init();
 console.log('We made context menus...')
-listenForConnections();
+//listenForConnections();
+// import {helloWorld} from './messaging/webMessages.js';
+// helloWorld.initializeSender();
+// setInterval(
+//     ()=>{
+//         console.log('Try to say hello?',helloWorld.ports.length,'ports')
+//         for (let tab in helloWorld.ports) {
+//             helloWorld.send(tab,`hello there ${tab}`);
+//         }
+//     },
+//     5000
+// );
