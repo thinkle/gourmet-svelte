848a790a0b1a0e3c723b0fe0a4b7321836ef8200
Tom Hinkle <tmhinkle@gmail.com>
2020-06-03 23:22:24 -0400

    mostly through reworking to use new messaging for ports

--- src/extension/web/Sidebar.svelte
+++ src/extension/web/Sidebar.svelte
@@ -1,7 +1,7 @@
 <script>
  import Tagger from './Tagger.svelte';
  import Views from './SidebarRecipeViews.svelte';
- import {onMount} from 'svelte';
+ import {onMount,getContext} from 'svelte';
  import {listenToExtension} from '../messaging/polling.js'
  import {backgroundParsePage,backgroundClearAll} from '../messaging/parsing.js';
 
@@ -10,6 +10,8 @@
  import Whisk from '../../widgets/WhiskLogo.svelte';
 
  import {parseData} from '../../importer/importer.js';
+ import {helloWorld,sendParsedToWeb,sendSelectionToWeb} from '../messaging/webMessages.js';
+
 
  // our data...
  let parsed;
@@ -19,27 +21,56 @@
  let firstConnect = true;
  // promises
  let parsing;
- 
+ let selectionActive;
+ let tb = getContext('toolbar');
+ if (tb) tb.hideWhenLoggedIn()
+ /*  let hello = 'I sure hope they say hello...' */
  onMount(
      ()=>{
-         let poller
-         console.log('Set up poller!');
-         poller = listenToExtension(
-             (msg)=>{
-                 console.log('Got poller message!',msg);
-                 parsed = msg.parsed
-                 if (firstConnect) {
-                     if (Object.keys(parsed).length < 2 ) {
-                         autoparsePage()
-                     }
-                     firstConnect = false;
-                 }
+         /* let helloListener = helloWorld.receive(
+          *     (message,port)=>{
+          *         console.log('Got hello',message,port)
+          *         hello = message;
+          *     }
+          * ); */
+         let disconnectParseListener = sendParsedToWeb.receive(
+             (message,port)=>{
+                 console.log('Got parsed message',message,port)
+                 parsed = message;
              }
          );
-         return function cleanUp () {
-             console.log('disconnecting poller');
-             poller.disconnect();
+         let disconnectSelectionListener = sendSelectionToWeb.receive(
+             (message,port)=>{
+                 console.log('Got selection message',message,port)
+                 selectionActive = message;
+             }
+         );
+         return ()=>{
+             disconnectSelectionListener();
+             disconnectParseListener();
          }
+         /* let messenger
+          * console.log('Set up poller!'); */
+         /* messenger = listenToExtension(
+          *     (msg)=>{
+          *         if (msg.type=='parsed') {
+          *             console.log('Got poller message!',msg);
+          *             parsed = msg.parsed
+          *             if (firstConnect) {
+          *                 if (parsed && Object.keys(parsed).length < 2 ) {
+          *                     autoparsePage()
+          *                 }
+          *                 firstConnect = false;
+          *             }
+          *         } else if (msg.type = 'selection') {
+          *             selectionActive = msg.active
+          *         }
+          *     }
+          * ); */
+         /* return function cleanUp () {
+          *     console.log('disconnecting poller');
+          *     messenger.disconnect();
+          * } */
      }
  );
 
@@ -57,13 +88,16 @@
 </script>
 
 <section>
-    <h2>Gourmet</h2>
+    <h2>
+        Gourmet
+    </h2>
     {#if parsed && parsed.pageInfo}
         <p>Importing {parsed.pageInfo.title}</p>
     {/if}
-
-    <span on:click="{()=>tagMode=true}">Tag</span>
-    <span on:click="{()=>tagMode=false}">View</span>
+    <div class="tabs" >
+    <span class:active="{tagMode}" on:click="{()=>tagMode=true}">Tag</span>
+    <span class:active="{!tagMode}" on:click="{()=>tagMode=false}">View</span>
+    </div>
 
     {#if parsing}
         {#await parsing}
@@ -106,5 +140,32 @@
  span:hover {
      text-decoration: underline;
  }
+ .tabs {
+     display: flex;
+     flex-direction: row;
+     background-color: white;
+     position: sticky;
+     top: 0;
+     background-color: white;
+     width: 100%;
+ }
+ .tabs span {
+     padding: 0.5em;
+     border-bottom : 1px solid var(--light-underline);
+     margin-right: 3px;
+     font-weight: 300; /* light */
+ }
+
+ .tabs span:hover {
+     border-bottom: 3px solid var(--medium-underline);
+     background-color: var(--light-bg);
+     color: var(--light-fg);
+     font-weight: 500;
+ }
+ .tabs .active {
+     border-bottom: 3px solid var(--heavy-underline);
+     font-weight: 500;
+ }
+
 </style>
     
