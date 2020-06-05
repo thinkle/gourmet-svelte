da16c6f9fdb4e4ceb5a2929a381c41b5fca138b0
Tom Hinkle <tmhinkle@gmail.com>
2020-06-05 14:09:10 -0400

    shh

--- src/importer/timeImporter.js
+++ src/importer/timeImporter.js
@@ -2,11 +2,10 @@ import {parse, end, toSeconds, pattern} from 'iso8601-duration';
 import Times from '../utils/times.js';
 import {parseAmount} from '../utils/numbers.js';
 import {cleanupWhitespace,titleCase} from '../utils/textUtils.js';
-import {ignoreMatchingChildren} from './importer.js';
+import {ignoreMatchingDescendants} from './importer.js';
 
 export function handleTime (chunk, context, recipe) {
-    ignoreMatchingChildren(chunk,context);
-    console.log('handleTime!',chunk)
+    ignoreMatchingDescendants(chunk,context);
     let amount;
     if (!amount) {
         amount = parseAmount(chunk.text);
@@ -21,13 +20,11 @@ export function handleTime (chunk, context, recipe) {
     }
     amount.text = cleanupWhitespace(chunk.text)
     if (chunk.html) {
-        console.log('parse...');
         let duration
         // Try parsing time...
         let doc = new DOMParser().parseFromString(chunk.html,'text/html');
         let candidate = doc.querySelector('*[datetime]')
         if (candidate) {
-            console.log('found candidate in DOM...');
             amount.seconds = toSeconds(parse(candidate['dateTime']));
             amount.iso8601 = candidate['dateTime']
         }
@@ -35,7 +32,6 @@ export function handleTime (chunk, context, recipe) {
     if (chunk.detail) {
         amount.name = chunk.detail
     }
-    console.log('Adding time: ',amount);
     if (!recipe.times) {
         recipe.times = []
     }
