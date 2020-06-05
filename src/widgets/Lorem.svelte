8fb83efc5444e2e8e8ac7445b4465edf7ecc100f
Tom Hinkle <tmhinkle@gmail.com>
2020-06-04 23:44:56 -0400

    ++convenience testing widget

---
+++ src/widgets/Lorem.svelte
@@ -0,0 +1,17 @@
+<script>
+ import ipsum from '../common/mocks/ipsum.js';
+ export let chars=undefined
+ export let graphs=undefined
+ let text
+ $: if (!graphs && !chars) {graphs = 2;}
+ $: if (!text && graphs) {text = ipsum.generateParagraphs(graphs)}
+ $: if (!text && chars) {text = ipsum.generate(chars)}
+ 
+ function changeIpsum () {
+     text = ''; // this will trigger reactive assignments to fix it
+ }
+
+</script>
+<div on:click={changeIpsum}>
+    {@html text}
+</div>
