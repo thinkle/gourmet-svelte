44dace253212eb267b115411e372dd9fea6cfb89
Tom Hinkle <tmhinkle@gmail.com>
2020-06-05 07:35:42 -0400

    shh

--- src/extension/background.js
+++ src/extension/background.js
@@ -5,36 +5,5 @@ import {launchTab} from './SidebarCommunication.js';
 import ContextMenus from './ContextMenus.js';
 console.log('Extension last built at BUILD_TIME')
 listen();
-console.log('Background loaded...');
 chrome.browserAction.onClicked.addListener(launchTab);
-console.log('We are listening...');
 ContextMenus.init();
-console.log('We made context menus...')
-//listenForConnections();
-// import {helloWorld} from './messaging/webMessages.js';
-// helloWorld.initializeSender();
-// setInterval(
-//     ()=>{
-//         console.log('Try to say hello?',helloWorld.ports.length,'ports')
-//         for (let tab in helloWorld.ports) {
-//             helloWorld.send(tab,`hello there ${tab}`);
-//         }
-//     },
-//     5000
-// );
-// import {hello} from './messaging/uiMessages.js';
-// console.log('Try to connect in 5...')
-// setTimeout(
-//     ()=>{
-//         hello.backgroundReceive(
-//             580,
-//             (message)=>{
-//                 console.log('Got message from 580:',message);
-//             }
-//         );},
-//     5000
-// );
-// setInterval(()=>{
-//     console.log('send a message back to content?')
-//     hello.send('Send a message back to content?',580);
-// },5500);
