7fd2f2473eab9d76aaec037578215a70b32d19ae
Tom Hinkle <tmhinkle@gmail.com>
2020-06-06 14:20:32 -0400

    ++demo

---
+++ src/widgets/Modal.demo.svelte
@@ -0,0 +1,33 @@
+<script>
+ import Button from './Button.svelte';
+ import Modal from './Modal.svelte';
+ import ModalLauncher from './ModalLauncher.svelte';
+ import Lorem from './Lorem.svelte';
+ let open=true;
+ let closeOnClickOutside=false;
+</script>
+<Button
+    toggle="true"
+    toggled="{closeOnClickOutside}"
+    on:click="{()=>closeOnClickOutside=!closeOnClickOutside}"
+>Close on Click Outside
+</Button>
+<p><Lorem inline="true" chars="{22}"/>
+    <ModalLauncher modalVisible="{open}"><button on:click="{()=>open=true}">Open</button></ModalLauncher></p>
+{#if open}
+    <Modal
+        {closeOnClickOutside}
+        width="600px"
+        onClose="{()=>open=false}">
+        <h3><Lorem inline="true" chars="30"/></h3>
+        <div>
+            <Lorem graphs="{30}"/>
+        </div>
+    </Modal>
+{/if}
+<Lorem graphs="{50}"/>
+
+
+<style>
+ h3 {font-size: 3rem;}
+</style>
