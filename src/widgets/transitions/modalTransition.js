9908c82507f687f8ac02085226d9e058c80da7e3
Tom Hinkle <tmhinkle@gmail.com>
2020-06-07 15:58:52 -0400

    rework transition

--- src/widgets/transitions/modalTransition.js
+++ src/widgets/transitions/modalTransition.js
@@ -1,5 +1,17 @@
 import { crossfade } from 'svelte/transition';
 
+
+let crossfades = {}
+
+export function getCrossfade (name, props={duration:200}) {
+    if (!crossfades[name]) {
+        crossfades[name] = crossfade({
+		duration: 200,
+	});
+    }
+    return crossfades[name]
+}
+
 const [send, receive] = crossfade({
 		duration: 200,
 	});
