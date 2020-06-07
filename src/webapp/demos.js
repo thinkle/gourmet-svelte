d272f8fe9f998e7281bb76c5fd8010e812d5f042
Tom Hinkle <tmhinkle@gmail.com>
2020-06-07 07:37:50 -0400

    Auto-generated demos.js file -- committing it here in case we later remove the auto-build from the build process for whatever reason

---
+++ src/webapp/demos.js
@@ -0,0 +1,38 @@
+import LocalData from "../data/LocalData.demo.svelte";
+import RemoteApi from "../data/RemoteApi.demo.svelte";
+import RemoteRecipe from "../data/RemoteRecipe.demo.svelte";
+import RecipeStores from "../stores/RecipeStores.demo.svelte";
+import Button from "../widgets/Button.demo.svelte";
+import IngredientInput from "../widgets/IngredientInput.demo.svelte";
+import Inputs from "../widgets/Inputs.demo.svelte";
+import Menu from "../widgets/Menu.demo.svelte";
+import Modal from "../widgets/Modal.demo.svelte";
+import SVGs from "../widgets/SVGs.demo.svelte";
+import Tabs from "../widgets/Tabs.demo.svelte";
+import Timer from "../widgets/Timer.demo.svelte";
+import Widgets from "../widgets/Widgets.demo.svelte";
+import Highlight from "../extension/parser/Highlight.demo.svelte";
+import RecipePicker from "../recDisplay/picker/RecipePicker.demo.svelte";
+import Recipe from "../recDisplay/rec/Recipe.demo.svelte";
+import RecipeText from "../recDisplay/text/RecipeText.demo.svelte";
+
+ 
+export default {
+    LocalData,
+    RemoteApi,
+    RemoteRecipe,
+    RecipeStores,
+    Button,
+    IngredientInput,
+    Inputs,
+    Menu,
+    Modal,
+    SVGs,
+    Tabs,
+    Timer,
+    Widgets,
+    Highlight,
+    RecipePicker,
+    Recipe,
+    RecipeText
+} 
