a46f68315e0f5f31d38b016a45eeb9f3fe7234ac
Tom Hinkle <tmhinkle@gmail.com>
2020-06-09 18:53:40 -0400

    Reorganize widgets into sub-directories

---
+++ src/widgets/index.js
@@ -0,0 +1,58 @@
+// Let's organize this baby...
+
+// *Controls
+// Button
+// IconButton
+// Resizer
+
+// *Inputs
+//AmountInput
+//CategoryInput
+//Checkbox
+//ComboInput
+//DurationInput
+//IngredientInput
+//MultiComboInput
+//NumberUnitInput
+//PlainInput
+//RangeInput
+//RichText
+//UrlInput
+
+// *Layout
+//Menu
+//MenuItem
+//Modal
+//ModalLauncher
+//SideBySide
+//Tab
+//Tabs
+
+// *Display
+//HtmlBlock
+//NumberUnitDisplay
+//Progress
+//Status
+//StatusIcon
+//TimeLink
+//Timer
+
+
+//? Forms?
+//SmallLabel
+
+// * Dev
+//JsonDebug
+//Lorem
+//Tester
+
+// FancyPants ?
+//SearchProgress
+//WhiskLogo
+
+// transitions
+export * from './controls/index.js';
+export * from './dev/index.js';
+export * from './display/index.js';
+export * from './inputs/index.js';
+export * from './layout/index.js';
