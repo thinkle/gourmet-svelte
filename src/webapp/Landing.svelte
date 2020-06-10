b22cdfbcdb882c819eacfea4e8a34a95da33f0ed
Tom Hinkle <tmhinkle@gmail.com>
2020-06-10 15:40:02 -0400

    troubleshooting netlifyIdentity widget...

--- src/webapp/Landing.svelte
+++ src/webapp/Landing.svelte
@@ -39,8 +39,15 @@
                             if ($redirectURL !== '') {
                                 console.log('Got redirect: ',$redirectURL);
                             }
+                            
      });
  }
+ netlifyIdentity.on('init', user => console.log('init', user));
+ netlifyIdentity.on('login', user => console.log('login', user));
+ netlifyIdentity.on('logout', () => console.log('Logged out'));
+ netlifyIdentity.on('error', err => console.error('Error', err));
+ netlifyIdentity.on('open', () => console.log('Widget opened'));
+ netlifyIdentity.on('close', () => console.log('Widget closed'));
 
  if (window.location.host.indexOf('localhost')>-1) {
      console.log('Fake login...');
