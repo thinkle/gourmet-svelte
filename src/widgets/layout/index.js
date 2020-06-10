a6ceac74a8339e3da60934b9aa5627d4dbd230ca
Tom Hinkle <tmhinkle@gmail.com>
2020-06-10 08:14:16 -0400

    new components

--- src/widgets/layout/index.js
+++ src/widgets/layout/index.js
@@ -1,7 +1,10 @@
-export {default as Menu} from './Menu.svelte';
+export {default as Bar} from './Bar.svelte';
+export {default as FullHeight} from './FullHeight.svelte';
+export {default as FullScreen} from './FullScreen.svelte';
 export {default as MenuItem} from './MenuItem.svelte';
-export {default as Modal} from './Modal.svelte';
+export {default as Menu} from './Menu.svelte';
 export {default as ModalLauncher} from './ModalLauncher.svelte';
+export {default as Modal} from './Modal.svelte';
 export {default as SideBySide} from './SideBySide.svelte';
-export {default as Tab} from './Tab.svelte';
 export {default as Tabs} from './Tabs.svelte';
+export {default as Tab} from './Tab.svelte';
