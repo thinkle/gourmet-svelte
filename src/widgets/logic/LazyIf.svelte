c43d580912fae6ebc2b9843850221d06c2c26239
Tom Hinkle <tmhinkle@gmail.com>
2020-06-10 08:13:53 -0400

    New Lazy Loading if block

---
+++ src/widgets/logic/LazyIf.svelte
@@ -0,0 +1,23 @@
+<script>
+ export let condition;
+ let shouldRender = false;
+ 
+ $: shouldRender = shouldRender || condition;
+ $: console.log('condition', condition);
+ $: console.log('shouldRender', shouldRender);
+</script>
+
+{#if shouldRender}
+    <div class="wrapper" class:active={condition}>
+	<slot />
+    </div>
+{/if}
+
+<style>
+ .wrapper {
+     display: none;
+ }
+ .wrapper.active {
+     display: contents;
+ }
+</style>
---
+++ src/widgets/logic/index.js
@@ -0,0 +1,1 @@
+export {default as LazyIf} from './LazyIf.svelte';
