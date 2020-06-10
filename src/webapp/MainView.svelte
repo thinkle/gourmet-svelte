7a7f58ca1b9fecdfd04614482c9e54fc2b900e2f
Tom Hinkle <tmhinkle@gmail.com>
2020-06-10 08:13:16 -0400

    Use new layout components.

--- src/webapp/MainView.svelte
+++ src/webapp/MainView.svelte
@@ -3,12 +3,19 @@
  import RecipeList from '../recDisplay/RecipeList.svelte';
  import ShoppingList from '../shopDisplay/ShoppingList.svelte';
  import {
+     Bar,
      Button,
      IconButton,
      JsonDebug,
+     LazyIf,
      ModalLauncher,
      Modal,
-     Status} from '../widgets/';
+     FullHeight,
+     Status,
+     Tabs,
+     Tab,
+     WhiskLogo,
+ } from '../widgets/';
  import OpenRecipes from '../recDisplay/OpenRecipes.svelte';
 
  import {connected,
@@ -24,75 +31,96 @@
  import {getContext,onMount} from 'svelte';
  import {writable} from 'svelte/store'
  let hideOpenButton = writable(true);
- let showShoppingList = false;
 
-
- let toolbar = getContext('toolbar');
-
- onMount(
-     ()=>{
-         if (!toolbar) {
-             console.log('No toolbar :(')
-             return
-         }
-         let toolbarItem = toolbar.addItem({
-             content : 'Shopping List',
-             modalVisible : false,
-             key:'ShoppingList',
-             props : {
-                 icon : 'shopping_cart',
-             },
-             'onClick' : ()=>showShoppingList=true
-         });
-         return toolbarItem.onUnmount
-     }
- );
-
+ let page='RecipeList'
 
 </script>
 
-<div>
-    {#if $connected}
-        <br>
-        <Button
-            on:click="{async ()=>opener.open(await recipeActions.createRecipe())}">
-            New Recipe
-        </Button>
-        {#if showShoppingList}
-            <Modal
-                key="ShoppingList"
-                width="1000px"
-                onClose="{()=>showShoppingList=false}">
-                <ShoppingList/>
-            </Modal>
-        {/if}
-    {/if}
+
+
+{#if $connected}
+    <Bar>
+
+        <div slot="left">
+            <Tabs>
+                <Tab
+                    active="{page=='RecipeList'}"
+                    on:click="{()=>page='RecipeList'}"
+                >
+                    Recipe List
+                </Tab>
+                <Tab
+                    active="{page=='ShoppingList'}"
+                    on:click="{()=>page='ShoppingList'}"
+                >
+                    Shopping List
+                </Tab>
+                {#if $openLocalRecipes.length > 0}
+                    <Tab active="{page=='OpenRecipes'}"
+                         on:click="{()=>page='OpenRecipes'}"
+                    >
+                        {#if $openLocalRecipes.length==1}
+                            View Recipe
+                        {:else}
+                            View Recipes ({$openLocalRecipes.length})
+                        {/if}
+                    </Tab>
+                {/if}
+            </Tabs>
+        </div>
+        <div slot="right">
+            <Button
+                on:click="{async ()=>opener.open(await recipeActions.createRecipe())}">
+                New Recipe
+            </Button>
+        </div>
+    </Bar>
+    <FullHeight scrolls="true">
+        <LazyIf condition="{page=='ShoppingList'}">
+            <ShoppingList/>
+        </LazyIf>
+        <LazyIf condition="{page=='RecipeList'}">
+            <RecipeList
+                onRecipeClick="{
+                               (id)=>localRecipes.open(id).then(()=>opener.open(id))
+                               }"
+            />
+        </LazyIf>
+        <OpenRecipes bind:this="{opener}"
+                     hide="{page!=='OpenRecipes'}"
+                     onOpen="{()=>page='OpenRecipes'}"
+        />
+    </FullHeight>
+    <Bar>
+        <div slot="center">
+            <Status/>
+        </div>
+        <div slot="right">
+            <button on:click="{()=>syncingPromise=recipeActions.doSync()}">Sync with Server?</button>
+            <button on:click="{()=>syncingPromise=recipeActions.doSync(true)}">Small Sync</button>
+        </div>
+        <div slot="left">
+            {#if syncingPromise}
+                {#await syncingPromise}
+                    Syncing...
+                {:then json}
+                    Cool, done syncing! {JSON.stringify(json)}
+                {:catch error}
+                    Failed :( {console.log(error)} {error}
+                {/await}
+            {/if}
+        </div>
+    </Bar>
+{:else}
     <div>
-        <OpenRecipes bind:this={opener}/>
-    </div>    
-    <RecipeList
-        onRecipeClick="{
-                       (id)=>localRecipes.open(id).then(()=>opener.open(id))
-                       }"
-    />
-    <Status/>
-    <button on:click="{()=>syncingPromise=recipeActions.doSync()}">Sync with Server?</button>
-    <button on:click="{()=>syncingPromise=recipeActions.doSync(true)}">Small Sync</button>
-    {#if syncingPromise}
-        {#await syncingPromise}
-            Syncing...
-        {:then json}
-            Cool, done syncing! {JSON.stringify(json)}
-        {:catch error}
-            Failed :( {console.log(error)} {error}
-        {/await}
-    {/if}   
-</div>
+        <WhiskLogo/>
+    </div>
+{/if}
 {#if DEV}
-<div>
-    Stored<JsonDebug data={$storedRecipes}/>
-    Local<JsonDebug data={$localRecipes}/>
-</div>
+    <div>
+        Stored<JsonDebug data={$storedRecipes}/>
+        Local<JsonDebug data={$localRecipes}/>
+    </div>
 {/if}
 <style>
 </style>
