593ab0e4952f6488b8b12b35340bfa42e3c8c739
Tom Hinkle <tmhinkle@gmail.com>
2020-06-11 00:22:12 -0400

    Fix scrolling for relative positioned children

--- src/utils/scrolling.js
+++ src/utils/scrolling.js
@@ -4,18 +4,35 @@ import { cubicOut } from 'svelte/easing';
 import scrollparent from 'scrollparent'
 
 export function scrollIntoView (node) {
-    let parent = scrollparent(node);
-    let scrollTop = tweened(parent.scrollTop,{
+    let origNode = node
+    let extraOffset = 0;
+    let scrollParent = scrollparent(node);
+    // If node is relative positioned, we have to climb...
+    if (node.offsetParent !== scrollParent) {
+        while (node && node.offsetParent && !(node.offsetParent && node.offsetParent.contains(scrollParent) && node.offsetParent !== scrollParent)) {
+            extraOffset += node.offsetTop;
+            node = node.offsetParent
+        }
+    }
+    if (!node) {
+        // That was weird... maybe another element will work.
+        return false;
+    }
+    let scrollTop = tweened(scrollParent.scrollTop,{
         duration : 1000,
         easing : cubicOut
     });
     let unsubscribe = scrollTop.subscribe(
-        (v)=>parent.scrollTop=v
+        (v)=>scrollParent.scrollTop=v
     );
-    //parent.scrollTop = (node.offsetTop - parent.clientHeight/2); // middle of parent
-    scrollTop.set(node.offsetTop - parent.clientHeight/2) // middle of parent
+    //scrollParent.scrollTop = (node.offsetTop - scrollParent.clientHeight/2); // middle of parent
+    
+    console.log('node offsetTop',node.offsetTop,'+',extraOffset,'parent height',scrollParent.clientHeight);
+    console.log('Target is: ',(node.offsetTop+extraOffset) - scrollParent.clientHeight/2);
+    scrollTop.set(node.offsetTop+extraOffset - scrollParent.clientHeight/2) // middle of parent
         .then(
             unsubscribe
         );
+    return true;
 }
 
