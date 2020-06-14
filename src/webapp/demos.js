018ed70e6293708067b42189a2d8bb70b570e14a
Tom Hinkle <tmhinkle@gmail.com>
2020-06-14 07:35:41 -0400

    Nevermind - put it back :)

---
+++ src/webapp/demos.js
@@ -0,0 +1,48 @@
+import User from "../account/User.demo.svelte";
+import LocalData from "../data/LocalData.demo.svelte";
+import RemoteApi from "../data/RemoteApi.demo.svelte";
+import RemoteRecipe from "../data/RemoteRecipe.demo.svelte";
+import ShoppingList from "../shopDisplay/ShoppingList.demo.svelte";
+import ShoppingListItems from "../shopDisplay/ShoppingListItems.demo.svelte";
+import RecipeStores from "../stores/RecipeStores.demo.svelte";
+import Widgets from "../widgets/Widgets.demo.svelte";
+import Highlight from "../extension/parser/Highlight.demo.svelte";
+import RecipePicker from "../recDisplay/picker/RecipePicker.demo.svelte";
+import Recipe from "../recDisplay/rec/Recipe.demo.svelte";
+import RecipeText from "../recDisplay/text/RecipeText.demo.svelte";
+import Button from "../widgets/controls/Button.demo.svelte";
+import Timer from "../widgets/controls/Timer.demo.svelte";
+import SVGs from "../widgets/display/SVGs.demo.svelte";
+import Checkbox from "../widgets/inputs/Checkbox.demo.svelte";
+import IngredientInput from "../widgets/inputs/IngredientInput.demo.svelte";
+import Inputs from "../widgets/inputs/Inputs.demo.svelte";
+import FullScreen from "../widgets/layout/FullScreen.demo.svelte";
+import Menu from "../widgets/layout/Menu.demo.svelte";
+import Modal from "../widgets/layout/Modal.demo.svelte";
+import Tabs from "../widgets/layout/Tabs.demo.svelte";
+
+ 
+export default {
+    User,
+    LocalData,
+    RemoteApi,
+    RemoteRecipe,
+    ShoppingList,
+    ShoppingListItems,
+    RecipeStores,
+    Widgets,
+    Highlight,
+    RecipePicker,
+    Recipe,
+    RecipeText,
+    Button,
+    Timer,
+    SVGs,
+    Checkbox,
+    IngredientInput,
+    Inputs,
+    FullScreen,
+    Menu,
+    Modal,
+    Tabs
+} 
