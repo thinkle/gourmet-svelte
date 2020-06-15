06d6441d9739affed1c1606f0ac5896e0b4c5813
Tom Hinkle <tmhinkle@gmail.com>
2020-06-14 22:15:12 -0400

    default icon fix

--- src/widgets/logic/FormTask.svelte
+++ src/widgets/logic/FormTask.svelte
@@ -1,7 +1,7 @@
 <script>
  export let name='Do task'
- export let buttonName='Submit'
- export let icon
+ export let buttonName=''
+ export let icon="save_alt"
  export let promiseAction=undefined
  export let onStart=undefined
  export let onComplete=undefined
@@ -34,7 +34,7 @@
  
 </script>
 
-<div class:boxy class:inline>
+<div class="container" class:boxy class:inline>
     <span class="head">{name}</span>
     <div class="form">
         <slot/>
@@ -66,6 +66,12 @@
      display: grid;
      grid-template-columns: 8rem auto;
      font-family: var(--uiFont);
+     grid-area: form;
+     align-items: center;     
+ }
+
+ .form :global(button) {
+     margin: auto;
  }
 
  .inline {
@@ -74,15 +80,23 @@
  .boxy {
      border: 1px solid var(--light-underline);
      padding: 1rem;
-     display: flex;
-     flex-direction: column;
+     display: grid;
+     grid-template-areas:
+         "title title"
+         "form  button"
+         ;
+     grid-template-columns: 1fr auto;
  }
 
- .boxy head {
-     font-size: 1.5em;
+ .boxy .head {
+     font-size: 1rem;
      font-weight: bold;
+     grid-area: title;
  }
  .submit {
-     margin-left: auto;
+     margin-left: 5px;
+     align-self: flex-end;
+     justify-self: flex-end;
+     grid-area: button;
  }
 </style>
