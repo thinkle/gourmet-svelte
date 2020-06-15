059053ac8013727f819a4f1475b04eb8d5fdd573
Tom Hinkle <tmhinkle@gmail.com>
2020-06-15 07:18:55 -0400

    ++indicate # of change events in tester

--- src/recDisplay/rec/Recipe.demo.svelte
+++ src/recDisplay/rec/Recipe.demo.svelte
@@ -1,17 +1,32 @@
 <script>
- import {Tester} from '../../widgets/';
+ import {Tester,Bar,Button} from '../../widgets/';
  import {testRecs} from '../../common/mocks/recipes.js'
  import Recipe from './Recipe.svelte';
  export let initialShow=true;
+ export let change=0
+ function onChange (v) {
+     console.log('Recipe change',v)
+     change+=1
+ }
 </script>
 
 <div> 
-    <Tester name="Test Recipe"
-            options={Object.keys(testRecs)}
-            let:option={recname}
-            initialShow={initialShow}
+    <Bar>
+        <div slot="center" >{change} changes...
+        </div>
+        <div slot="right">
+            <Button on:click={()=>change=0}>Reset</Button>
+        </div>
+    </Bar>
+    <Tester
+        name="Test Recipe"
+        options="{Object.keys(testRecs)}"
+        let:option={recname}
+              initialShow="{initialShow}"
     >
-        <Recipe rec={testRecs[recname]}>
+        <Recipe rec="{testRecs[recname]}"
+                onChange="{onChange}"
+        >
         </Recipe>
     </Tester>
 </div>
