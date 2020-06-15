486abc5bd4573236548c289d79d2af51047b5275
Tom Hinkle <tmhinkle@gmail.com>
2020-06-15 17:02:08 -0400

    ++sort ++pretty

--- src/data/LocalData.demo.svelte
+++ src/data/LocalData.demo.svelte
@@ -1,17 +1,26 @@
 <script>
- import localApi from '../data/dexieApi.js';
- 
+ import localApi from './dexieApi.js';
+ import {JsonDebug,Button} from '../widgets/'
  async function addRec () {
-     results = await localApi.addRecipe({title:'Hello world'});
+     results = await localApi.addRecipe({title:'Hello world',savedRemote:0});
  }
  let results
+ let sortProp='title'
 </script>
 
 
-<button on:click={()=> localApi.connect()} >Connect</button>
-<button on:click={()=>addRec()} >Add Rec</button>
-<button on:click={async ()=>results= await localApi.getRecipe(1)} >Get Rec #1</button>
-<button on:click={async ()=>results= await localApi.getRecipes()}>Get recipes</button>
-<pre>{results && JSON.stringify(results)}</pre>
+<br><Button on:click={()=> localApi.connect()} >Connect</Button>
+<br><Button on:click={()=>addRec()} >Add Rec</Button>
+<br><Button on:click={async ()=>results= await localApi.getRecipe(1)} >Get Rec #1</Button>
+<br><Button on:click={async ()=>results= await localApi.getRecipes()}>Get recipes</Button>
+<br><Button on:click="{async ()=>results= await localApi.getRecipes(
+                 {limit:50,
+                  query:{deleted:0},
+                  sort:sortProp}
+                 )}">Get sorted recs</Button> <input bind:value="{sortProp}">
+<br><Button on:click={async ()=>{debugger;results= await localApi.getRecipes({
+                 query:{savedRemote:0}
+                 })}}>Get unsynced</Button>
+<JsonDebug data="{results}"/>
 
 
