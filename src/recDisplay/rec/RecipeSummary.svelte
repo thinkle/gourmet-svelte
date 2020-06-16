eb3f26fb82607bc1adf6a094f221a18f255b4a58
Tom Hinkle <tmhinkle@gmail.com>
2020-06-16 16:57:51 -0400

    move thumb to right

--- src/recDisplay/rec/RecipeSummary.svelte
+++ src/recDisplay/rec/RecipeSummary.svelte
@@ -12,24 +12,34 @@
      }
  }
 </script>
-    <td class="clickable title" on:click={wowAClick}>
-        <h3>{recipe.title}</h3>
+<td class="clickable title" on:click={wowAClick}>
+    <h3>{recipe.title}</h3>
+</td>
+<td class="thumb" style={`--maxImageWidth:${maxImageWidth}px`}>
+    {#if thumb}
+        <div >
+            <img src={thumb.url} alt={thumb.alt||recipe.title}/>
+            <!-- Forget the thumbnails for now -->
+        </div>
+    {/if}
+</td>
+{#each RecDef.recProps.filter((p)=>p.summaryView) as prop}
+    <td class="{prop.name}">
+        <RecPropDisplay prop={prop} value={recipe[prop.name]}></RecPropDisplay>
     </td>
-    {#each RecDef.recProps.filter((p)=>p.summaryView) as prop}
-        <td class="{prop.name}">
-            <RecPropDisplay prop={prop} value={recipe[prop.name]}></RecPropDisplay>
-        </td>
-    {/each}        
-    <td class="thumb" style={`--maxImageWidth:${maxImageWidth}px`}>
-        {#if thumb}
-            <img src={thumb.thumbnailUrl||thumb.url} alt={thumb.alt||recipe.title}/>
-        {/if}
-    </td>
+{/each}        
 <style>
  .clickable:hover {
      text-decoration: underilne;
  }
  img {
-     max-width: var(--maxImageWidth)
+    min-width: var(--maxImageWidth);
+    max-height: var(--maxImageWidth);
+    }
+ div {
+     border-radius: 50%;
+     width: var(--maxImageWidth);
+     height: var(--maxImageWidth);
+     overflow: hidden;
  }
 </style>
