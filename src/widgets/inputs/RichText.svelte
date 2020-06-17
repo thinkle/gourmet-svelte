e493a500b61f362f398bc32bf7fd940dfeb9fe67
Tom Hinkle <tmhinkle@gmail.com>
2020-06-17 15:29:34 -0400

    ++font

--- src/widgets/inputs/RichText.svelte
+++ src/widgets/inputs/RichText.svelte
@@ -22,7 +22,7 @@
              toolbar: toolbarOptions
          },
          theme: "snow",
-         placeholder: "Write your story..."
+         placeholder: "Type text here..."
      });
      quill.pasteHTML(value);
      quill.on('text-change', function(delta) {
@@ -47,5 +47,24 @@
     </div>
 </div>
 <style>
- 
+ div div div :global([contenteditable]) {
+     border: none;
+ }
+ div div div {
+     min-height: 15em;
+     border: 1px solid #ccc;
+ }
+
+ div :global([contenteditable]) {
+     font-family: var(--recipeFont);
+     font-size : 1rem;
+     line-height: 1.4;
+     max-width: 45em; /* readability... */
+ }
+
+ div :global(h1),div :global(h2),div :global(h3),div :global(h4),div :global(h5),div :global(h6) {
+     margin-bottom: 3px;
+     border-bottom: 1px solid var(--light-underline);
+ }
+
 </style>
