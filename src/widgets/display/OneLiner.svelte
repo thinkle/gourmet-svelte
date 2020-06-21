b64ef09013d081b51aefab8ee5fdd3572bbff23e
Tom Hinkle <tmhinkle@gmail.com>
2020-06-21 12:11:31 -0400

    ++OneLiner convenience component for clipping with ellipsis and showing overflow on hover

---
+++ src/widgets/display/OneLiner.svelte
@@ -0,0 +1,39 @@
+<script>
+ export let bg = 'var(--white)';
+ export let fg = 'var(--black)';
+
+</script>
+<span class="container">
+    <span class="overflow" style="{`color:${fg};background-color:${bg};`}"><slot/></span>
+    <span class="oneline"><slot/></span>
+</span>
+
+<style>
+ .container {
+     position: relative;
+ }
+ span.overflow {
+     display: none;
+     position: absolute;
+     /* top: 0;
+        left: 0; */
+     z-index: 2;
+ }
+ span:hover .overflow {
+     display: inline-block;
+ }
+
+ span.oneline {
+     display: inline-block;
+     width: 100%;
+     text-overflow: ellipsis;
+     overflow-x: hidden;
+     min-height: 1.2em; /* make room for descenders, working around annoying display bug... */ 
+     white-space: nowrap;
+ }
+ span:hover .oneline {
+     visibility: hidden;
+ }
+ 
+</style>
+    
