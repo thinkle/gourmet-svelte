b5a8fa1d428f9676c9e2c5c00e3e3846b41c522f
Tom Hinkle <tmhinkle@gmail.com>
2020-06-21 12:20:00 -0400

    widget to scan times in text

---
+++ src/recDisplay/rec/TimeSummary.svelte
@@ -0,0 +1,50 @@
+<script>
+ export let rec
+
+ import {TimeLink,JsonDebug} from '../../widgets/';
+ import {extractTimes} from '../../utils/times.js';
+
+ let times = []
+
+ $: times = getTimes(rec);
+ 
+ function getTimes (rec) {
+     times = [...rec.times]
+     for (let text of rec.text) {
+         const extracted = extractTimes(text.body);
+         console.log('extracted ',extracted.length,'from text',text);
+         times = [...times,...extracted]
+     }
+     return times
+ }
+
+ 
+</script>
+
+<div>
+    {#each times as time}
+        <!-- <JsonDebug data={time}/>
+             <br>text: {time.text}
+             <br>secs: {time.seconds}
+             <br>sentence : {time.sentence} -->
+        {#if time.name}
+            <TimeLink
+                value="{time}"
+            />
+        {:else}
+            <TimeLink
+                label="{time.sentence}"
+                seconds="{time.seconds}"
+                timestring="{time.text}"
+            />
+        {/if}
+    {/each}
+</div>
+<style>
+ div {
+     display: grid;
+     grid-template-columns: 49% 1px 50%;   /* WARNING: Tied to internals of TimeLink.svelte  */
+     grid-row-gap: 1em;
+     grid-column-gap: 5px;
+ }
+</style>
