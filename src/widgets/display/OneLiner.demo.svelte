685938f80a80e61c323c4a28900ee4c046e40315
Tom Hinkle <tmhinkle@gmail.com>
2020-06-21 12:11:23 -0400

    ++OneLiner convenience component for clipping with ellipsis and showing overflow on hover

---
+++ src/widgets/display/OneLiner.demo.svelte
@@ -0,0 +1,37 @@
+<script>
+ import OneLiner from './OneLiner.svelte';
+ import {Lorem} from '../'
+</script>
+
+<div style="width:200px">
+    <p>In a 200px div</p>
+    <OneLiner>
+        <Lorem chars="{300}"/>
+    </OneLiner>
+</div>
+<div class="grid" style="width:200px">
+    <p>In a grid layout</p>
+    <OneLiner>
+        <Lorem chars="{300}"/>
+    </OneLiner>
+    <OneLiner>
+        <Lorem chars="{300}"/>
+    </OneLiner>
+    <OneLiner>
+        <Lorem chars="{300}"/>
+    </OneLiner>
+    <OneLiner>
+        <Lorem chars="{300}"/>
+    </OneLiner>
+    <OneLiner>
+        <Lorem chars="{300}"/>
+    </OneLiner>
+</div>
+
+<style>
+ .grid {
+     display: grid;
+     grid-template-columns: 150px 150px 150px;
+     grid-spacing: 5px;
+ }
+</style>
