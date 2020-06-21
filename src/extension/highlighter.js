cf9e841e025539318d3315f4e36e76047663cf75
Tom Hinkle <tmhinkle@gmail.com>
2020-06-21 12:16:22 -0400

    ++highlighter

---
+++ src/extension/highlighter.js
@@ -0,0 +1,80 @@
+import IngHighlight from '../recDisplay/text/IngHighlight.svelte';
+import {parseIngredients} from '../utils/ingredientUtils.js';
+import {parseTimes} from '../utils/times.js';
+import {writable} from 'svelte/store';
+import {contentSetIngredients,contentHighlightIng, highlightedIngredientsCB} from './messaging/highlighterMessages.js';
+
+let highlightedIngredients = writable({
+    active:[]
+});
+let justSet = false;
+highlightedIngredientsCB.contentReceive(
+    ($highlightedIngredients)=>{
+        justSet = true;
+        highlightedIngredients.set($highlightedIngredients)
+    }
+);
+
+highlightedIngredients.subscribe(
+    ($highlightedIngredients)=>{
+        if (justSet) {
+            console.log('Ignore our own setting event');
+            justSet = false;
+        } else {
+            console.log('highlightedIngredients set from content side UI, update background')
+            highlightedIngredientsCB.send(
+                $highlightedIngredients
+            );
+        }
+    }
+);
+
+//setContext('highlightedIngredient',{highlighted:'sugar'})
+
+
+contentSetIngredients.receive(
+    (ingredients)=>highlight(ingredients)
+);
+
+export async function highlight (ingredients) {
+    await crawlAndHighlight(document.body,ingredients);
+}
+
+export async function crawlAndHighlight (node,ingredients) { // async to not block...
+    if (node.children) {
+        for (let child of node.children) {
+            await crawlAndHighlight(child,ingredients);
+        }
+    }
+    if (node.innerHTML == node.innerText) {
+        // leaf node...
+        let parsed = parseIngredients(node.innerHTML, ingredients)
+        if (parsed && parsed != node.innerHTML) {
+            console.log('Replacing!');
+            node.innerHTML = parsed;
+            let toInsert = []
+            for (let insertedNode of node.children) {
+                if (insertedNode.tagName=='ING') {
+                    toInsert.push(insertedNode);
+                }
+            }
+            toInsert.forEach(
+                (insertedNode)=>{
+                    console.log('insert one...');
+                    
+                    let ih = new IngHighlight({
+                        target:node,
+                        anchor:insertedNode,
+                        props:{
+                            highlightedIngredientFromProps:highlightedIngredients,
+                            target:insertedNode.getAttribute('target'),
+                            content:insertedNode.textContent,
+                            useStyle:true
+                        }
+                    });
+                    insertedNode.remove();
+                }
+            );
+        }
+    }
+}
---
+++ src/extension/highlighterBackground.js
@@ -0,0 +1,51 @@
+import {highlightedIngredientsCB, sendHighlightToWeb, backgroundHighlightIng,
+        contentSetIngredients,backgroundSetIngredients,
+       } from './messaging/ingredientHighlighter.js';
+
+// Go between between web and content...
+export function initHighlighterBackground () {
+    console.log('set up background script go-between for passing highlighter messages between web and content');
+
+    backgroundSetIngredients.receive(
+        (ingredients,sender)=>{
+            console.log('bg pass along ingredients to content',ingredients,sender,sender.tab);
+            contentSetIngredients.send(
+                ingredients,
+                sender.tab
+            );
+            return true;
+        },
+        true // external
+    );
+
+
+    sendHighlightToWeb.initializeSender();
+    sendHighlightToWeb.onWebConnect(
+        // When web connects to us...
+        async (tabid) => {
+            console.log('Connected highlighter to web: tab',tabid)
+            // We pass all content messages on to the web
+            highlightedIngredientsCB.backgroundReceive(
+                tabid,
+                (message)=>{
+                    console.log('content says highlight, pass it on',message);
+                    sendHighlightToWeb.send(tabid,message)
+                }
+            );
+        }
+    );
+
+
+    // Also the other way...
+    console.log('Set up backgroundHighlightIng receiver...')
+    backgroundHighlightIng.receive(
+        async (msg, sender) => {
+            // pass message along...
+            console.log('web says highlight, pass it on',msg,sender);
+            highlightedIngredientsCB.send(msg,sender.tab.id);
+            return true;
+        },
+        true // external
+    );
+    console.log('Done w/ background highlighter setup');
+}
