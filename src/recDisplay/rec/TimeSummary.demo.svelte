b720b4e76484330cacf1b27a6fcd41957fbf9b55
Tom Hinkle <tmhinkle@gmail.com>
2020-06-21 12:19:54 -0400

    widget to scan times in text

---
+++ src/recDisplay/rec/TimeSummary.demo.svelte
@@ -0,0 +1,23 @@
+<script>
+import TimeSummary from './TimeSummary.svelte'
+ import {Tester,Bar,Button,FullHeight,FullScreen} from '../../widgets/';
+ import {testRecs} from '../../common/mocks/recipes.js'
+ import Recipe from './Recipe.svelte';
+ export let initialShow=true;
+ export let change=0
+ function onChange (v) {
+     console.log('Recipe change',v)
+     change+=1
+ }
+</script>
+
+<Tester
+  name="Test Recipe"
+  options="{Object.keys(testRecs)}"
+  let:option="{recname}"
+  initialShow="{initialShow}"
+>
+  <TimeSummary rec="{testRecs[recname]}"
+                onChange="{onChange}"
+        />
+    </Tester>
