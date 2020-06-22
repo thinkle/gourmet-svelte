0c5ebc9207a4e7fcb0450b25ae97357fbdf0c735
Tom Hinkle <tmhinkle@gmail.com>
2020-06-22 17:36:31 -0400

    get basic test up and running

--- src/recDisplay/rec/Recipe.test.js
+++ src/recDisplay/rec/Recipe.test.js
@@ -1,23 +1,50 @@
 import RecipeDemo from './Recipe.demo.svelte'
+import {tick} from 'svelte';
 import { render, fireEvent } from '@testing-library/svelte'
+import userEvent from '@testing-library/user-event'
 import { testRecs } from '../../common/mocks/recipes.js'
 import deepcopy from 'deepcopy';
+let defaultRec = 'standard'
+let standard = testRecs[defaultRec]
 
-it('Recipe renders', () => {
-    render(RecipeDemo);
-    
-    //render(Recipe)
-  // //  const increment = getByText('increment')
-  // //  const decrement = getByText('decrement')
-  // const counter = getByTestId('counter-value')
-  
-  // await fireEvent.click(increment)
-  // await fireEvent.click(increment)
-  // await fireEvent.click(increment)
-  // await fireEvent.click(decrement)
 
-  // expect(counter.textContent).toBe('2')
+function reduceWS (text) {
+    return text
+        .replace(/\s+/g,' ') // reduce whitespace to single space
+        .replace(/^\s+|\s+$/g,''); // remove leading/trailing
+}
 
-  // with jest-dom
-    //expect(counter).toHaveTextContent('2')
+// How do we do decorators in JS?
+function _getByContainingText (getByText) {
+    return function (text) {
+        text = reduceWS(text);
+        return getByText((content, node) => {
+            const hasText = node => reduceWS(node.textContent) === text
+            const nodeHasText = hasText(node);
+            const childrenDontHaveText = Array.from(node.children).every(
+                child => !hasText(child)
+            );
+            return nodeHasText && childrenDontHaveText;
+        })
+    }
+}
+
+
+it('Recipe renders and ingredients multiply', async () => {
+    const {getByText,
+           getByLabelText,
+           getAllByText,
+           container,
+           component} = render(RecipeDemo);
+    const getByContainingText = _getByContainingText(getByText);
+    let titleElement = getByText(standard.title)
+    let categoryElement = getByText(standard.categories[0].name)
+    // We have 3 cups diced and peeled potato
+    // hard-coded search here
+    let potatoes = getByContainingText('3 cups diced and peeled potato')
+    let multiplier = getByLabelText('multiply by')
+    await userEvent.type(multiplier,'{backspace}2');
+    //await tick();
+    //expect(multiplier.textContent).toMatch(/2/)
+    expect(potatoes.textContent).toMatch(/6/)
 })
