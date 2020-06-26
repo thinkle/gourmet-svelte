5687307620727b9f737f1a407337819b3c967e4a
Tom Hinkle <tmhinkle@gmail.com>
2020-06-26 12:00:53 -0400

    move to new request api

--- src/data/functions/recipeFunctions.test.js
+++ src/data/functions/recipeFunctions.test.js
@@ -4,8 +4,13 @@ const requestHandlers = {
 registerHandlerObject(requestHandlers);
 
 import deepcopy from 'deepcopy';
-import recipeFunctions,{addRecipe,
-                        getMostRecent} from './recipeFunctions.js';
+import {addRecipe,
+        deleteRecipe,                        
+        getRecipe,
+        getRecipes,
+        getMostRecent,
+        updateRecipe,
+       } from './recipeFunctions.js';
 import {testRecs} from '../../common/mocks/recipes.js';
 import user,{otherUser} from '../../common/mocks/user.js';
 import {setupDBwithRecs} from './setupMockDB.js'
@@ -27,8 +32,8 @@ it(
         expect(recipe.owner.email).toEqual(user.email)
         expect(recipe.title).toEqual(testRecs.standard.title);
         expect(recipe.ingredients.length).toEqual(testRecs.standard.ingredients.length);
-        let fetchedRec = await recipeFunctions.getRecipe(
-            ...bp,
+        let fetchedRec = await getRecipe(
+            user,
             {_id:recipe._id}
         );
         expect(fetchedRec.title).toEqual(testRecs.standard.title);
@@ -40,12 +45,41 @@ it(
     }
 )
 
+it(
+    'patchRecipe',
+    async () => {
+        let r = deepcopy(testRecs.standard)
+        let recipe = await addRecipe(user,{recipe:r});
+        let patchedRecipe = await updateRecipe(
+            user,
+            {
+                recipe : {
+                    _id:recipe._id,
+                    ingredients:[{text:'new ingredient',amount:{unit:'tsp',amount:7}}],
+                    categories:[{name:'Snack Time'},{name:'Easy Peasy'}],
+                },
+                forceMerge : true
+            }
+        );
+        console.log('ings:',JSON.stringify(patchedRecipe.ingredients));
+        console.log('cats:',JSON.stringify(patchedRecipe.categories));
+        expect(patchedRecipe.categories.length).toEqual(recipe.categories.length + 2)
+        let titlePatch = await updateRecipe(
+            user, {
+                recipe: {_id:recipe._id,title:'Foo'},
+                forceMerge:true
+                
+            })
+        expect(titlePatch.title).toEqual('Foo');
+        expect(titlePatch.categories.length).toEqual(patchedRecipe.categories.length);
+    }
+);
 
 it(
     'updateRecipe',
     async ()=> {
         let r = deepcopy(testRecs.standard)
-        let recipe = await recipeFunctions.addRecipe(...bp,{recipe:r});
+        let recipe = await addRecipe(user,{recipe:r});
         recipe.ingredients.push(
             testRecs.testIngredient
         );
@@ -53,8 +87,8 @@ it(
             testRecs.testIngredient
         );
         recipe.title += ' test change'
-        let modifiedRecipe = await recipeFunctions.updateRecipe(...bp,
-                                                                {recipe});
+        let modifiedRecipe = await updateRecipe(user,
+                                                {recipe});
         let oldCopy = deepcopy(modifiedRecipe);
         expect(modifiedRecipe._id).toEqual(recipe._id);
         expect(modifiedRecipe.title).toEqual(testRecs.standard.title+' test change');
@@ -64,7 +98,7 @@ it(
         let first_save_time = modifiedRecipe.last_remote_save;
         modifiedRecipe.title += 'AGAIN!';
         modifiedRecipe.categories.push({name:'Other Category'});
-        let modified2 = await recipeFunctions.updateRecipe(...bp,{recipe:modifiedRecipe});
+        let modified2 = await updateRecipe(user,{recipe:modifiedRecipe});
         console.log('M1:',first_save_time,'M2:',modified2.last_remote_save)
         expect(modified2.last_remote_save).not.toEqual(first_save_time);
         expect(modified2.merged).toBeFalsy()
@@ -72,14 +106,14 @@ it(
         oldCopy.ingredients.push({text:'Some new thing!',amount:{amount:2,unit:'TBS'}});
         oldCopy.ingredients[0].ingredients.push({text:'Foof',amount:{amount:7,unit:'whippersnaps'}});
         oldCopy.categories.push({name:'new catgry!'});
-        let modified3 = await recipeFunctions.updateRecipe(...bp,{recipe:oldCopy});
+        let modified3 = await updateRecipe(user,{recipe:oldCopy});
         //console.log('ok... ingredients are',JSON.stringify(modified3.ingredients));
         expect(modified3.merged).toBeTruthy();
         expect(modified3.ingredients.length).toEqual(modified2.ingredients.length+1)
         expect(modified3.ingredients[0].ingredients.length).toEqual(modified2.ingredients[0].ingredients.length+1)
         expect(modified3.categories.length).toEqual(modified2.categories.length+1)
         modified3.ingredients = [] // delete everything!
-        let modified4 = await recipeFunctions.updateRecipe(...bp,{recipe:modified3});
+        let modified4 = await updateRecipe(user,{recipe:modified3});
         expect(modified4.merged).toBeFalsy();
         expect(modified4.ingredients.length).toEqual(0)
         expect(modified4.categories.length).toEqual(modified3.categories.length); // no change there..
@@ -90,14 +124,14 @@ it(
 it(
     "enforce user's only modify own data",
     async () => {
-        let rec = await recipeFunctions.addRecipe(...bp,{recipe:{title:'Private Recipe'}});
-        expect(async ()=>await recipeFunctions.getRecipe(...bp,{_id:rec._id}))
+        let rec = await addRecipe(user,{recipe:{title:'Private Recipe'}});
+        expect(async ()=>await getRecipe(user,{_id:rec._id}))
             .not
             .toThrowError();
         let threwError = false;
         try {
-            let stolenRec = await recipeFunctions.getRecipe(event,context,otherUser,
-                                                            {_id:rec._id});
+            let stolenRec = await getRecipe(otherUser,
+                                            {_id:rec._id});
             console.log('I stole the cookie jar: ',stolenRec);
         }
         catch (err) {
@@ -108,8 +142,8 @@ it(
         let origRecId = rec._id
         threwError = false;
         try {
-            let muckedUpRec = await recipeFunctions.updateRecipe(
-                event,context,otherUser,
+            let muckedUpRec = await updateRecipe(
+                otherUser,
                 {recipe:rec}
             );
         } catch (err) {
@@ -120,8 +154,8 @@ it(
             expect(muckedUpRec._id).not.toEqual(origRecId)
         } 
         try {
-            let deletedOtherPersonsRecipes = await recipeFunctions.deleteRecipe(
-            event,context,otherUser,
+            let deletedOtherPersonsRecipes = await deleteRecipe(
+                otherUser,
                 {_id:rec._id}
             );
             expect(deletedOtherPersonsRecipes).toEqual(0)
@@ -143,15 +177,15 @@ async function expectError (f) {
     expect('Error').toEqual('have been thrown');
 }
 
-it('nice errors',
+xit('nice errors', // moved this stuff to new paradigm
    async () => {
        let e = await expectError(
-           async ()=>await recipeFunctions.getRecipe(...bp,{recipe:{title:'foo'}})
+           async ()=>await getRecipe(user,{recipe:{title:'foo'}})
        );
        expect(e.message).toMatch(/foo/) // should report what parameters we passed with
        expect(e.message).toMatch(/_id/) // should report what parameters were needed
        let e2 = await expectError(
-           async ()=>await recipeFunctions.updateRecipe(...bp,{recipe:{title:'foo'}})
+           async ()=>await updateRecipe(user,{recipe:{title:'foo'}})
        );
        expect(e2.message).toMatch(/foo/) // should report what parameters we passed with
        expect(e2.message).toMatch(/_id/) // should report what parameters were needed
@@ -162,15 +196,15 @@ it('nice errors',
 it(
     'delete recipe',
     async () => {
-        let rec = await recipeFunctions.addRecipe(...bp,{recipe:{title:'Bad Recipe'}});
+        let rec = await addRecipe(user,{recipe:{title:'Bad Recipe'}});
         let id = rec._id;
-        let gotItBack = await recipeFunctions.getRecipe(...bp,{_id:id});
+        let gotItBack = await getRecipe(user,{_id:id});
         expect(gotItBack._id).toEqual(id);
-        let deleteCount = await recipeFunctions.deleteRecipe(...bp,{_id:id});
+        let deleteCount = await deleteRecipe(user,{_id:id});
         expect(deleteCount).toEqual(1)
         let err = await expectError(
             async ()=>{
-                let backFromTheDead = await recipeFunctions.getRecipe(...bp,{_id:id});
+                let backFromTheDead = await getRecipe(user,{_id:id});
                 console.log('VERY BAD: GOT REC BACK FROM THE DEAD',backFromTheDead)
             }
         );
@@ -188,24 +222,24 @@ describe('pagination', ()=>{
     it(
         'pagination',
         async () => {
-            let result = await recipeFunctions.getRecipes(
-                ...bp,
+            let result = await getRecipes(
+                user,
                 {limit:10})
             expect(result).toBeDefined();
             expect(result.count).toBeDefined()
             expect(result.page).toBeDefined()
             expect(result.result).toBeDefined()
             expect(result.result.length).toEqual(10);
-            let nextResult = await recipeFunctions.getRecipes(
-                ...bp,
+            let nextResult = await getRecipes(
+                user,
                 {limit:10,page:result.page}
             );
             expect(result.result.indexOf(nextResult.result[0])).toEqual(
                 -1
             );
             let lastPage = result.count - 1;
-            let lastResults = await recipeFunctions.getRecipes(
-                ...bp,
+            let lastResults = await getRecipes(
+                user,
                 {limit:10,page:lastPage}
             );
             expect(lastResults.result.length).toBeLessThan(10);
