75c7f5e1417c64e558e8e90997c158a7b5c4407e
Tom Hinkle <tmhinkle@gmail.com>
2020-06-26 12:00:27 -0400

    border / focus highlighting when in editing mode

--- src/recDisplay/props/RecProp.svelte
+++ src/recDisplay/props/RecProp.svelte
@@ -84,20 +84,41 @@
                 <label class:small={smallLabel} on:click={()=>ref.focus()}>{prop.label}</label>
                 {#if editable && !edit}
                     <span class="editbutton" >
-                        <IconButton small={smallLabel} icon="edit" bare="true" on:click={turnEditOn}/>
+                        <IconButton
+                            small="{smallLabel}"
+                            icon="edit"
+                            bare="true"
+                            ariaLabel="{`Edit ${prop.label}`}"
+                            on:click={turnEditOn}
+                        />
                     </span>
                 {/if}
                 {#if edit && !forceEdit}
-                    <IconButton small={smallLabel} icon="done" bare="true" on:click={turnEditOff}/>
+                    <IconButton small="{smallLabel}"
+                                icon="done"
+                                bare="true"
+                                ariaLabel="{`Finish editing ${prop.label}`}"
+                                on:click={turnEditOff}/>
                 {/if}
             </div>
         {:else if editable && !forceEdit}
             <div class='floatingEditButton'>
                 {#if editable && !edit}
-                    <span class="editbutton" ><IconButton bare="true" icon="edit" on:click={turnEditOn}/></span>
+                    <span class="editbutton" >
+                        <IconButton
+                            bare="true"
+                            icon="edit"
+                            ariaLabel="{`Edit ${prop.label}`}"
+                            on:click={turnEditOn}
+                        />
+                    </span>
                 {/if}
                 {#if edit && !forceEdit}
-                    <IconButton icon="done" bare="true" on:click={turnEditOff}/>
+                    <IconButton
+                        icon="done"
+                        bare="true"
+                        ariaLabel="{`Finish editing ${prop.label}`}"                        
+                        on:click={turnEditOff}/>
                 {/if}
             </div>
         {/if}
@@ -134,7 +155,7 @@
      position: fixed;
      right: 300px;
      top: 350px;
-    
+     
  }
  label {
      font-family: var(--recipeHeadFont);
@@ -182,6 +203,7 @@
  }
  .small {
      font-size: var(--small);
+     line-height: 2; /* Make sure out descenders show up... */
  }
  
  .block .floatingEditButton {
@@ -191,19 +213,39 @@
  .title {
      display: flex;
      flex-direction: row-reverse;
-     white-space: normal; /* Allow wrapping on title, even in navbar */
  }
+ @media (min-width: 600px) {
+     .title {
+         white-space: normal; /* Allow wrapping on title, even in navbar */
+     }
+ }
+ 
  .editing.title {
      flex-direction: column;
  }
 
  .block.editing {
      width : var(--available-width);
+     border-color: #eee;
+     transition: border-color 300ms;
+ }
+ .block.editing:focus-within {
+     border-color: #888;
+     transition: border-color 300ms;
+ }
+ .block.editing:focus-within label {
+     font-weight: bold;
+ }
+ 
+ .block {
+     border: 1px solid transparent;
+     padding: 3px;
  }
 
  .fullWidth.block.editing {
      clear: right;
      width: 100%;
+     box-sizing: border-box;
  }
  div {
      flex-grow: 1;
