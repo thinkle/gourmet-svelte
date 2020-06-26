205ed296a8ff4630130db28d388f8f164b7b0195
Tom Hinkle <tmhinkle@gmail.com>
2020-06-26 11:56:16 -0400

    ++magic underline thing

---
+++ src/widgets/inputs/Underline.svelte
@@ -0,0 +1,52 @@
+<script>
+ export let grow=true;
+</script>
+
+<span class:grow>
+    <slot/>
+</span>
+
+<style>
+
+ .grow {
+     width: 100%;
+     flex-grow: 1;
+ }
+
+ span {
+     display: inline-block;
+     border-bottom: var(--inputBorder);
+     padding-bottom: var(--inputPadding);
+     position: relative;
+     margin-bottom: var(--inputPadding);
+     box-sizing: content-box;
+ }
+
+ span :global(input),
+ span :global([contenteditable]) {
+     border: none;
+     border-color: transparent;
+ }
+
+ span::after {
+     content: " ";
+     display: block;
+     width: 0;
+     height: 2px;
+     position: absolute;
+     bottom: 0;
+     background-color: var(--inputBorderColor);
+ }
+
+ span:focus-within::after {
+     content: " ";
+     display: block;
+     width: 100%;
+     height: 2px;
+     position: absolute;
+     bottom: 0;
+     transition: all 300ms;
+     background-color: var(--accent-bg);
+ }
+ 
+</style>
