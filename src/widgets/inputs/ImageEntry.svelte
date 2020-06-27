050c02bc886210ee11bbecf8c034e0ab3b0d4c38
Tom Hinkle <tmhinkle@gmail.com>
2020-06-26 23:11:18 -0400

    ++Image upload

---
+++ src/widgets/inputs/ImageEntry.svelte
@@ -0,0 +1,177 @@
+<script>
+ export let url;
+ export let value;
+ export let maxSize=1200;
+ 
+ import { tick,createEventDispatcher } from 'svelte';
+ const dispatch = createEventDispatcher();
+
+ import {IconButton,Underline} from '../'
+ 
+ let fullUrl
+ let existingImage
+ import pica from 'pica';
+ let p = pica()
+ let targetWidth = 0;
+ let targetHeight = 0;
+
+ if (value) {
+     console.log('Image got value:',value);
+     url = value && value.url
+     console.log('URL is',url);
+ }
+
+ function onExistingLoaded () {
+     targetWidth = existingImage.clientWidth;
+     targetHeight = existingImage.clientHeight;
+     console.log('FETCH WIDTH',targetWidth);
+ }
+         
+ $: if (url && value) {
+     // reactive update
+     value.url = url;
+     dispatch('change',value)
+ }
+
+ function updateUrl(event) {
+     const file = event.target.files[0];
+     const reader = new FileReader();
+
+     reader.addEventListener("load", async function () {
+         // convert image file to base64 string
+         fullUrl = reader.result;
+         url = ''
+         targetWidth = undefined;
+         targetHeight = undefined;
+         console.log('Using pica...',p);         
+     }, false);
+     
+     if (file) {
+         reader.readAsDataURL(file);
+     }
+ }
+
+ async function onFQLoaded () {
+     console.log('Full image is',fullQuality.clientWidth,fullQuality.clientHeight);
+     let options = {
+     }
+     if (fullQuality.clientWidth < maxSize &&
+         fullQuality.clientHeight < maxSize) {
+         url = fullUrl;
+     } else {
+         if (fullQuality.clientWidth  > fullQuality.clientHeight) {
+             targetWidth = maxSize;
+             targetHeight = targetWidth * (fullQuality.clientHeight/fullQuality.clientWidth);
+             
+         } else {
+             targetHeight = maxSize;
+             targetWidth = maxSize * (fullQuality.clientWidth/fullQuality.clientHeight);
+         }
+         console.log('resize to',targetWidth,targetHeight);
+         await tick();
+         let result = await p.resize(
+             fullQuality,
+             resizeCanvas,
+             {toWidth:targetWidth,
+             toHeight:targetHeight}
+         )
+         console.log('Done resizing');
+         console.log('Got result',result)
+         url = result.toDataURL()
+     }
+ }
+ 
+ let fullQuality;
+ let resizeCanvas;
+ let fileMode=false;
+ let elementWidth;
+
+ let renderWidth, renderHeight;
+
+ $: {
+     if (elementWidth && targetWidth) {
+         if (targetWidth < elementWidth) {
+             renderWidth = targetWidth;
+             renderHeight = targetHeight;
+         } else {
+             renderWidth = elementWidth - 30;
+             renderHeight = renderWidth * (targetHeight/targetWidth);
+         }
+     }
+ }
+ 
+</script>
+<div bind:clientWidth={elementWidth} class="ruler"></div>
+<div class="image" style="{url && `width:${renderWidth}px;height:${renderHeight}px;background-size:contain;background-image:url(${url});`}" class:hasImage="{url}">
+    <div class="inputs">
+        <label>
+            {#if url}
+                Replace Image
+            {:else}
+                Add Image
+            {/if}
+        </label>
+        {#if fileMode}
+            <input
+                accept="image/*"
+                type="file"
+                on:change="{updateUrl}"
+            >
+        {:else}
+            URL: <Underline grow="{true}"><input style="width:100%;background:transparent;" type="text" bind:value="{url}"/></Underline>
+        {/if}
+        <span class="right">
+            <IconButton
+                icon="attach_file"
+                toggle="{true}"
+                toggled="{fileMode}"
+                on:click="{()=>fileMode=!fileMode}"/>
+        </span>
+    </div>
+</div>
+{#if url}
+    <img style="{`max-width:${maxSize}px;max-height:${maxSize}px`}" on:load={onExistingLoaded} class="fq" bind:this={existingImage} src={url}>
+{/if}
+{#if fileMode}
+    <canvas bind:this="{resizeCanvas}" width="{targetWidth}" height="{targetHeight}"></canvas>
+    <img class="fq" on:load="{onFQLoaded}" bind:this="{fullQuality}" src="{fullUrl}">
+{/if}
+
+
+
+<style>
+ .ruler {
+     height: 0px;
+     overflow: hidden;
+ }
+ .image {
+     display: flex;
+     max-width: calc(100% - 2em);
+ }
+ .right {
+     margin-left: auto;
+ }
+
+ .inputs {
+     display: flex;
+     flex-grow: 1;
+ }
+
+ .hasImage .inputs {
+     background-color: #ffffffc7;
+     margin-top: auto;
+ }
+ img {
+     position: absolute;
+     top: 0;
+     left: 0;
+ }
+ canvas {
+     visibility: hidden;
+     position: absolute;
+ }
+ .fq {
+     visibility: hidden;
+     position: absolute;
+ }
+</style>
