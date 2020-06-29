1589a2815005043b76312df3e83d90825ef3f5d7
Tom Hinkle <tmhinkle@gmail.com>
2020-06-29 07:58:04 -0400

    ++loremflickr default + display value

--- src/widgets/inputs/Image.demo.svelte
+++ src/widgets/inputs/Image.demo.svelte
@@ -1,8 +1,21 @@
 <script>
  import ImageEntry from './ImageEntry.svelte'
- let url
+ let url = "https://loremflickr.com/1920/1080"
+ let value={}
 </script>
 
-Image Widget: <ImageEntry bind:url="{url}" />
-
+Image Widget: <ImageEntry on:change="{(e)=>value=e.detail}" bind:url="{url}" />
+<div>
+    VALUE:
+    value.url: {value.url && value.url.substr(0,100)}
+    value.caption: {value.caption}
+    value.float: {value.float}
+    value.width: {value.width}
+</div>
 And the image is...? <img src="{url}">
+
+<style>
+    div {
+    height: 100px;
+    }
+</style>
