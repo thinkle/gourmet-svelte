3200c07b429207cd6b4df6a15a3afc4812fd1d25
Tom Hinkle <tmhinkle@gmail.com>
2020-06-29 13:25:14 -0400

    ++color schemes!!

--- src/recDisplay/rec/colors.js
+++ src/recDisplay/rec/colors.js
@@ -2,6 +2,16 @@ import Color from 'color';
 let color
 
  export const wordColors = [
+     // easter eggs...
+     {matcher:/easter/i, color : {
+         fg:'#6434b2',
+         bg:'#dcf9a8'
+     }},
+     {matcher:/halloween/i, color : {
+         bg:'#1f0905',
+         fg:'#f97306',
+     }},
+
      // some whimsical ingredient matchers
      {matcher:/eggplant|aubergine|gh?anou/i, color: {bg:'#614051',fg:'#ffdcff'}},
      {matcher:/chocolate/i, color: {bg:'#5c3317',fg:'#e3cbb5'}},
@@ -10,22 +20,26 @@ let color
      {matcher:/asparagus/i, color : {bg: '#87a96b', fg:'#edfdef'}},
      {matcher:/spinach/i, color: {bg:'#455439',fg:"#deffcd"}},
      {matcher:/apple/i, color: {bg:'#ff0800',fg:'#ffdfee'}},
-     {matcher:/tomato/i, color: {bg:'#ff6347',fg:'#ffdfdf'}},
+     {matcher:/tomato/i, color: {bg:'#9f0404',fg:'#ffd4c2'}},
+     {matcher:/lemon/i,color:{bg:'#fff04d',fg:'#368321'}},
+
      
      // courses
      {matcher:/cookie|bread/i, color:{bg:'#fdcf4c',fg:'#4c2307'}}, // beige
      {matcher:/dessert/i, color: {bg:'#FF3366',fg:'#ffdede'}}, // red/pink
-     {matcher:/salad/i, color: {bg:'#55ab55',
-                                fg:'#ddffdd'
+     {matcher:/salad/i, color: {bg:'#bdeabd',
+                                fg:'#133f13'
                                }}, // green
 
      // cuisines...
+     {matcher:/greek|greece|mediterr/i, color: {bg:'#0154C7',fg:'#ebf1ff'}},
      {matcher:/pasta|italy|italian/i, color: {bg:'#ce2b37',fg:'#fedeed'}},
      {matcher:/india/i, color: {bg:'#FEB200',fg:'black'}}, // tumeric
-     {matcher:/thai/i, color: {bg:'#f94b1e',fg:'#ffffce'}}, // red/orange
+     {matcher:/thai/i, color: {bg:'#fff194',fg:'#db5800'}}, // red/orange
      {matcher:/china|chinese|asian/i, color: {bg:'#c51015',fg:'#F8D803'}}, // red/orange
      {matcher:/french|france/i, color: {bg:'#456990',fg:'#ffc501'}}, // queen blue
-     {matcher:/mexic|spanish|spain/i, color: {bg:'#831E10',fg:'#ffdfdf'}}, // persimmon
+     {matcher:/mexic/,color:{bg:'#a55409',fg:'#ffe294'}},     
+     {matcher:/spanish|spain/i, color: {bg:'#FED76A',fg:'#6A1410'}}, 
      
  ]
 wordColors.forEach(
