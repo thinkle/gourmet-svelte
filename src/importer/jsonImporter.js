7ad41a340b232b63f899f05fc3fa2d20fd04668a
Tom Hinkle <tmhinkle@gmail.com>
2020-07-04 15:44:13 -0400

    ++json

---
+++ src/importer/jsonImporter.js
@@ -0,0 +1,108 @@
+import {handleChunk} from './importer.js';
+
+export function handleLDJson (chunk,context,recipe) {
+    try {
+        var json = JSON.parse(chunk.fullText);
+    } catch (err) {
+        console.log('invalid json:',chunk.fullText);
+        return
+    }
+    if (Array.isArray(json)) {
+        json.map((o)=>handleLDObj(o,context,recipe))
+    } else {
+        handleLDObj(json,context,recipe);
+    }
+}
+
+function handleLDObj (json, context) {
+    // We are basically a complete importer on our own, so we actually make a completely
+    // new import for our context.... we will let the importer merge all the separate
+    // imports at the end...
+    if (!context.separateImports) {
+        context.separateImports = []
+    }
+    const recipe = {
+        images : [],
+        sources : [],
+        yields : [],
+        categories : [],
+        text : [],
+        times : [],
+        ingredients : [],
+    }
+    context.separateImports.push(recipe)
+    if (json['@type'] == 'Recipe') {
+        if (json.name) {recipe.title = json.name}
+        if (json.url) {
+            recipe.sources.push({url:json.url})
+        }
+        if (json.author) {
+            recipe.sources = [...recipe.sources,...json.author]
+        }
+        if (json.recipeCuisine) {
+            recipe.categories = [...recipe.categories,{name:json.recipeCuisine,type:'cuisine'}]
+        }
+        if (json.recipeCategory) {
+            recipe.categories = [...recipe.categories,{name:json.recipeCategory}]
+        }
+        if (json.recipeYield) {
+            handleChunk(
+                {text:json.recipeYield,
+                 tag:'yields'},
+                context,recipe
+            )
+        }
+        // times
+        for (let [prop,name] of [['cookTime','Cooking Time'],
+                                 ['totalTime','Total Time'],
+                                 ['prepTime','Preparation Time']
+                                ]) {
+            if (json[prop]) {
+                if (json[prop][0]=='P') {
+                    handleChunk(
+                        {text:' ',iso8601:json[prop],
+                         detail:name,
+                         tag:'time'},
+                        context,
+                        recipe
+                    )
+                } else {
+                    handleChunk(
+                        {tag:'time',
+                         text:json[prop],
+                         detail:name},
+                        context,
+                        recipe
+                    )
+                }
+            }
+        }
+        if (json.recipeInstructions) {
+            if (Array.isArray(json.recipeInstructions)) {
+                json.recipeInstructions.map(
+                    ({text})=>{
+                        handleChunk(
+                            {text,
+                             tag:'text'},
+                            context,
+                            recipe
+                        );
+                    }
+                );
+            }
+        }
+        if (json.recipeIngredient) {
+            if (Array.isArray(json.recipeIngredient)) {
+                json.recipeIngredient.map(
+                    (i)=>handleChunk({text:i,tag:'ingredient'},
+                                     context,
+                                     recipe)
+                )
+            }
+        }
+        if (json.image) {
+            recipe.images = [...recipe.images,json.image]
+        }
+    }
+    return context
+}
