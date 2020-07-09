9830f53fbb4d5baa95c22eb3f8f59b47b90db856
Tom Hinkle <tmhinkle@gmail.com>
2020-07-09 08:44:02 -0400

    shh

--- src/widgets/layout/tabCrossfade.js
+++ src/widgets/layout/tabCrossfade.js
@@ -5,10 +5,8 @@ const faders = {
 
 export function getCrossfade(k) {
     if (faders[k]) {
-        console.log('Using crossfader',k);
         return faders[k];
     } else {
-        console.log('Creating crossfader for ',k)
         faders[k] = crossfade({
             duration:600,
             fallback:fade
