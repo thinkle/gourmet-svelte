882586779aaec702c03a06c94818a5709e8d0c59
Tom Hinkle <tmhinkle@gmail.com>
2020-07-12 22:46:17 -0400

    inject content so we don't need content permissions for all tabs

--- src/extension/SidebarCommunication.js
+++ src/extension/SidebarCommunication.js
@@ -4,16 +4,11 @@ export function launchTab (tab) {
     console.log('browser action triggered: launch tab');
     chrome.tabs.executeScript(
         tab.id,
-        {file:'embed.js'}, // inject sidebar code, which listens for a message w/ id
-        // function () { // send message with id
-        //     chrome.tabs.sendMessage(tab.id,
-        //                             {tabId:tab.id});
-        //     if (parsed[tab.id]) {
-        //         // clear out parsed rec
-        //         parsed[tab.id] = {}
-        //     }
-        //     chrome.tabs.sendMessage(tab.id,
-        //                             {action:'parsePage'});
+        {file:'content.js'}
+    );
+    chrome.tabs.executeScript(
+        tab.id,
+        {file:'embed.js'},
     );
 }
 
