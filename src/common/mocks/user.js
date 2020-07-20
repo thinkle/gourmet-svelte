35273a771c49888b8c8f09f6b09dbcf9b65cf0fc
Tom Hinkle <tmhinkle@gmail.com>
2020-07-20 17:15:52 -0400

    ++logged in/out users for testing mocks

--- src/common/mocks/user.js
+++ src/common/mocks/user.js
@@ -2,8 +2,50 @@ export default {
     id : 1,
     name : 'Joe Shmoe',
     email : 'foo@bar.baz',
-    account:  'foo@bar.baz'
+    account:  'foo@bar.baz',
+    remoteUser : {
+        name : 'Joe Shmoe',
+        email : 'foo@bar.baz',
+        account:  'foo@bar.baz',
+        dbUser : {
+            newUser : false,
+        }
+    }
 }
+
+export const newUser = {
+    id : 1,
+    name : 'Joe Shmoe',
+    email : 'foo@bar.baz',
+    account:  'foo@bar.baz',
+    remoteUser : {
+        name : 'Joe Shmoe',
+        email : 'foo@bar.baz',
+        account:  'foo@bar.baz',
+        dbUser : {
+            newUser : true,
+        }
+    }
+}
+
+export const sharingUser = {
+    id : 1,
+    name : 'Sharing Shmoe',
+    email : 'foo@bar.baz',
+    account:  'foo@bar.baz',
+    remoteUser : {
+        name : 'Joe Shmoe',
+        email : 'foo@bar.baz',
+        account:  'foo@bar.baz',
+        dbUser : {
+            newUser : false,
+            linkedAccounts : ['foo@boo.bar','boo@bang.baz']
+        }
+    }
+}
+
+
+
 export const otherUser = {
     id : 2,
     name : 'Fred Shormgashborg',
