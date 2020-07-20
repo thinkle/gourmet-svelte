37680ee9f6cd5cb2473f48ec01f0a457e0e5cf44
Tom Hinkle <tmhinkle@gmail.com>
2020-07-20 17:15:35 -0400

    ++fake login for testing

--- src/account/User.demo.svelte
+++ src/account/User.demo.svelte
@@ -1,4 +1,43 @@
 <script>
-    import User from './User.svelte';
+ import u,{newUser,sharingUser} from '../common/mocks/user.js';
+ import {user} from '../stores/userStore.js';
+ import User from './User.svelte';
+ import {setContext} from 'svelte';
+
+ setContext('login',
+            {
+                doLogin () {
+                    console.log('login!');
+                    user.fake(u)
+                },
+                doSignup () {
+                },
+                doLogout () {
+                    console.log('logout!');
+                    user.logout();
+                }
+ });
+
 </script>
-<User/>
+
+<div>
+    <User/>
+</div>
+<div>
+    <button on:click="{
+                      ()=>user.fake(u)
+                      }">
+        Default User
+    </button>
+
+    <button on:click="{
+                      ()=>user.fake(newUser)
+                      }">
+        New User
+    </button>
+    <button on:click="{
+                      ()=>user.fake(sharingUser)
+                      }">
+        Sharing User
+    </button>
+</div>
