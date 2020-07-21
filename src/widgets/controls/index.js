b149b78f466c98064b199bb6ac05512f989d4254
Tom Hinkle <tmhinkle@gmail.com>
2020-07-21 11:53:16 -0400

    ++download

--- src/widgets/controls/index.js
+++ src/widgets/controls/index.js
@@ -1,4 +1,5 @@
 export {default as Button} from './Button.svelte';
+export {default as DownloadButton} from './DownloadButton.svelte';
 export {default as IconButton} from './IconButton.svelte';
 export {default as Resizer} from './Resizer.svelte';
 export {default as Timer} from './Timer.svelte';
