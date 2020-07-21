434676e762ccce06e99cda581d52a54835777c3b
Tom Hinkle <tmhinkle@gmail.com>
2020-07-21 01:00:26 -0400

    ++import

---
+++ src/data/functions/importRecipeFunction.js
@@ -0,0 +1,13 @@
+import {importRecipesRequest} from '../requests/index.js';
+import {prepRecsRemote} from '../validate.js'
+import {insertMany} from './mongoConnect.js';
+
+importRecipesRequest.setRequestHandler(importRecipes);
+
+async function importRecipes (user, params) {
+    let collection = params;
+    prepRecsRemote(collection,user);
+    let importedRecipes = await insertMany('recipes',collection.recipes);
+    return importedRecipes;
+}
+export default 'loaded';
