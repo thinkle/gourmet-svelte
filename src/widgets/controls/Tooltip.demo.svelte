5a914acbc90f0ba9f6ad993bbf166cf3951b688c
Tom Hinkle <tmhinkle@gmail.com>
2020-07-22 20:53:23 -0400

    ++demo

--- src/widgets/controls/Tooltip.demo.svelte
+++ src/widgets/controls/Tooltip.demo.svelte
@@ -1,42 +1,42 @@
 <script>
-	import Tooltip from './Tooltip.svelte'
-	import {tooltip} from './tooltip.js'
-	let show
-	let spans = ['Cool beans','Awesome sauce','Fan-nnn-tastic']
+ import {tooltip} from './tooltip.js'
+ let show
+ let spans = ['Cool beans','Awesome sauce','Fan-nnn-tastic']
 </script>
+
 <h2>
-	{#each spans as span}
+    {#each spans as span}
 	<span use:tooltip="{{content:`${span}This one uses an action`}}">{span}</span>
-	{/each}
+    {/each}
 </h2>
 
 <p use:tooltip={{content:"this one is absolute positioned"}}>
-	Foof
+    Foof
 </p>
 
 <blockquote >
-	<div  use:tooltip={{content:"this one is on the top of the bottom"}}>
-		Bottom 0
-	</div>
-	<div  use:tooltip={{content:"this one is on the  bottom"}}>
-		Bottom 1
-	</div>
-	<div use:tooltip={{content:"this one is on the very bottom"}}>		
-		Bottom 2
-	</div>
+    <div  use:tooltip={{content:"this one is on the top of the bottom"}}>
+	Bottom 0
+    </div>
+    <div  use:tooltip={{content:"this one is on the  bottom"}}>
+	Bottom 1
+    </div>
+    <div use:tooltip={{content:"this one is on the very bottom"}}>		
+	Bottom 2
+    </div>
 </blockquote> 
 <style>
-	blockquote {
-		position: fixed;
-		bottom: 0;
-		margin: 0;
-	}
-	p {
-		position: absolute; 
-		top: 100px; 
-		right: 0;
-		width: 100px;
-		height: 100px;
-		border: 2px solid #f00;
-	}
+ blockquote {
+     position: fixed;
+     bottom: 0;
+     margin: 0;
+ }
+ p {
+     position: absolute; 
+     top: 100px; 
+     right: 0;
+     width: 100px;
+     height: 100px;
+     border: 2px solid #f00;
+ }
 </style>
