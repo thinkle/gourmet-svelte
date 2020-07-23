694d1a1980ee84ba6da02c6a44834c95049dd2c6
Tom Hinkle <tmhinkle@gmail.com>
2020-07-22 20:54:02 -0400

    export tooltip

--- src/widgets/controls/index.js
+++ src/widgets/controls/index.js
@@ -3,3 +3,4 @@ export {default as DownloadButton} from './DownloadButton.svelte';
 export {default as IconButton} from './IconButton.svelte';
 export {default as Resizer} from './Resizer.svelte';
 export {default as Timer} from './Timer.svelte';
+export {tooltip} from './tooltip.js';
