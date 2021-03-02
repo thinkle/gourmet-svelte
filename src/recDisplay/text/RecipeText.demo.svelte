d4bde567e79e8bdc314bd5ab61bab7c832d563da
Tom Hinkle <tmhinkle@gmail.com>
2021-03-02 12:10:59 -0500

    Include punctuation to highlight whitespace handling

--- src/recDisplay/text/RecipeText.demo.svelte
+++ src/recDisplay/text/RecipeText.demo.svelte
@@ -1,11 +1,11 @@
 <script>
  import RecipeText from './RecipeText.svelte';
- let hidden=true;
+ let hidden=false;
  export let initialShow
  let value = {
      header : 'Instructions',
      body :` How about a 5 second timer for testing?<br>
-<p>These are instructions. Bake for 30 minutes at 350 degrees.</p>
+<p>These are instructions. Bake for 30 minutes, or maybe 35 minutes, or whatever happens to commas in 10 seconds, at 350 degrees.</p>
 <p>Are some of them getting lost?</p>
 <p>Then cook for 25 minutes</p>
      `,
