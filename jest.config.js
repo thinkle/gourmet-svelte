2e604833a70a78731b561d79773a2b80ed49fce4
Tom Hinkle <tmhinkle@gmail.com>
2021-03-22 22:34:15 -0400

    simplify launching/testing

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,32 @@
 let sassStuff = require('svelte-preprocess-sass')
 let sass = sassStuff.sass
 
+
+
+// https://stackoverflow.com/questions/60323002/svelte-3-could-not-import-modules-when-unit-testing
+
+
+console.log('Loaded jest config!');
+const svelteJestPreprocessor = () => ({
+  // replace `import foo from 'foo'` -> `import * as foo from 'foo'`
+    script: ({ content }) => {
+        console.log('Preprocess',content);
+        return {
+    // process each line of code
+      code: content.split('\n')
+          .map(line => {
+               // pass: no import, import with {}, import svelte component
+               if ((!line.match(/\s*import/)) || (line.match(/{/)) || (line.match(/\.svelte/))) {
+                   return line
+               } else {
+                   console.log('preprocess replace!',line,line.replace(/import/, 'import * as'));
+                   return line.replace(/import/, 'import * as');
+               }
+          }).join('\n')
+        }
+    }
+});
+
 module.exports = {
     "testEnvironment":"jest-environment-jsdom-fourteen",
     "setupFiles": [
@@ -15,12 +41,17 @@ module.exports = {
     transform : {
         //'^.+\\.svelte$' : 'svelte-jester',
         '^.+\\.svelte$' : ['jest-transform-svelte', {
-            preprocess: {
+            preprocess: [                
+                svelteJestPreprocessor(),
+                {
                 style: sass({}, { name: 'scss' }),
-            },
+                },
+
+            ],
         }],
         '^.+\\.js$' : 'babel-jest',
     },
     moduleFileExtensions : ['js','svelte'],
-    "preset": "@shelf/jest-mongodb"
+    //"preset": "@shelf/jest-mongodb"
 }
+
